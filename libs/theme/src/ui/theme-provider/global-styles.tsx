097e0a2e140ca9945c015b34f3111c1be64b240c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-04-12 17:50:07 -0300

    feat: moved global styles

---
+++ libs/theme/src/ui/theme-provider/global-styles.tsx
@@ -0,0 +1,11 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+export const globalStyles = <GlobalStyles styles={{
+  "html, body, #root": {
+    minHeight: "100%",
+  },
+  body: {
+    padding: 0,
+    height: "100%"
+  }
+}} />
--- libs/theme/src/ui/theme-provider/theme-provider.tsx
+++ libs/theme/src/ui/theme-provider/theme-provider.tsx
@@ -1,29 +1,19 @@
 import { theme } from '../../lib/theme';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import GlobalStyles from '@mui/material/GlobalStyles';
 import { PropsWithChildren } from 'react';
+import { globalStyles } from './global-styles';
 
 type Props = {
   withGlobalStyles?: boolean;
 }
-
-const global = <GlobalStyles styles={{
-  "html, body, #root": {
-    minHeight: "100%",
-  },
-  body: {
-    padding: 0,
-    height: "100%"
-  }
-}} />
 
 export function ThemeProvider({ children, withGlobalStyles = true }: PropsWithChildren<Props>) {
   return (
     <MUIThemeProvider theme={theme}>
       <>
         <CssBaseline />
-        {withGlobalStyles && global}
+        {withGlobalStyles && globalStyles}
         {children}
       </>
     </MUIThemeProvider>
