a6518f3c417170dc57487c9cc2e534751c68299e
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-04-12 22:27:29 -0300

    feat: added brand colors

---
+++ libs/theme/src/lib/config/colors.ts
@@ -0,0 +1,47 @@
+import type { PaletteColorOptions } from '@mui/material/styles/createPalette';
+
+export const purple: PaletteColorOptions= {
+  main: '#9A53FF',
+  dark: '#6421CB',
+  light: '#D083FF',
+};
+export const grays: PaletteColorOptions= {
+  main: '#E5E5E5',
+  dark: '#B3B3B3',
+  light: '#FFFFFF',
+};
+export const background: PaletteColorOptions= {
+  main: '#170627',
+};
+export const red: PaletteColorOptions= {
+  main: '#FF002E',
+  dark: '#C30006',
+  light: '#FF5A58',
+};
+export const green: PaletteColorOptions= {
+  main: '#6DFFB9',
+  dark: '#2CCB89',
+  light: '#81C784',
+};
+export const orange: PaletteColorOptions= {
+  main: '#FFA726',
+  dark: '#F57C00',
+  light: '#FFB74D',
+};
+export const blue: PaletteColorOptions= {
+  main: '#0075FF',
+  dark: '#004BCB',
+  light: '#4FC3F7',
+}
+
+const brandColors= {
+  purple,
+  grays,
+  background,
+  red,
+  green,
+  orange,
+  blue
+};
+
+export default brandColors;
--- libs/theme/src/lib/config/palette.ts
+++ libs/theme/src/lib/config/palette.ts
@@ -1,35 +1,21 @@
 import type { PaletteOptions } from '@mui/material/styles/createPalette';
+import brandColors from "./colors"
 
-const colors = {
-  purple: {
-    main: '#170627',
-    dark: '#10041b',
-    light: '#453752',
-  },
-  blue: {
-    dark: '#170627',
-  },
-};
 
 const palette: PaletteOptions = {
   mode: 'dark',
   primary: {
-    ...colors.purple,
-  },
-  secondary: {
-    main: '#FE02B9',
-    dark: '#b10181',
-    light: '#fe34c7',
+    ...brandColors.purple,
   },
   background: {
-    default: colors.blue.dark,
+    default: brandColors.background.main,
   },
   text: {
-    primary: '#efefef',
+    primary: brandColors.grays.main,
   },
   action: {
-    hover: colors.purple.main,
-    active: colors.purple.main,
+    hover: brandColors.purple.main,
+    active: brandColors.purple.main,
   },
 };
 
