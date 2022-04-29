09b906a6a29912f285f544742a72f358d5d5df68
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-04-29 14:40:57 -0300

    chore: try to remove yarn from ignore apps

--- tools/scripts/build-ignore-apps.sh
+++ tools/scripts/build-ignore-apps.sh
@@ -2,14 +2,6 @@
 # Name of the app to check. Change this to your application name!
 APP=$1
 
-# Determine version of Nx installed
-NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
-TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
-
-# Install @nrwl/workspace in order to run the affected command
-yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline
-yarn add -D typescript@$TS_VERSION --prefer-offline
-
 # Run the affected command, comparing latest commit to the one before that
 yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
