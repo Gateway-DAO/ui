7bae551595b237406e4769974ed0cf349e8d310d
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-05-03 17:05:40 -0300

    feat: created style-constants

---
+++ apps/website/components/atoms/style-tokens/style-tokens.ts
@@ -0,0 +1,7 @@
+import { ResponsiveStyleValue } from '@mui/system';
+
+/**
+ * Reusable style tokens for responsiveness acorss the website.
+ */
+
+export const CONTAINER_PX: ResponsiveStyleValue<number> = [2, 4, null, 6];
