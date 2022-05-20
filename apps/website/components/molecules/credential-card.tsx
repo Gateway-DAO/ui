d35806fbb9185078f7a325ef373e8bdab59a29c0
fbenfraj <farouk.ben-fraj@outlook.com>
2022-05-20 15:15:01 +0200

    Add credential card + modal space between elements

---
+++ apps/website/components/molecules/credential-card.tsx
@@ -0,0 +1,32 @@
+import { Chip } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function CredentialCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="302"
+        image="https://i.ibb.co/Lz8f6Mq/olympus-credential-picture.png"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" fontSize={20}>
+          Olympus Operations Work...
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          The Operations Group at Olympus is responsible for making sure that
+          work...
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Chip label="Operations" />
+        <Chip label="Contributor" />
+      </CardActions>
+    </Card>
+  );
+}
--- apps/website/components/organisms/poc-modal-created/poc-modal-created.tsx
+++ apps/website/components/organisms/poc-modal-created/poc-modal-created.tsx
@@ -7,11 +7,12 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 
+import CredentialCard from '../../molecules/credential-card';
+
 const style = {
   position: 'absolute' as const,
   top: '50%',
   left: '50%',
-  height: '100vh',
   minWidth: '100%',
   transform: 'translate(-50%, -50%)',
   bgcolor: 'background.paper',
@@ -44,54 +45,67 @@ export default function PocModalCreated({ open, handleClose }) {
             }}
             onClick={handleClose}
           />
-          <Typography
-            id="modal-modal-title"
-            variant="h6"
-            component="h2"
-            fontSize={48}
-            textAlign="center"
-          >
-            Proof of Credential created
-          </Typography>
-          <Typography
-            id="modal-modal-description"
-            sx={{ mt: 2, textAlign: 'center' }}
-            fontSize={16}
-          >
-            You have created the{' '}
-            <span style={{ color: '#D083FF' }}>
-              Olympus Operations Working Group - Season 2
-            </span>{' '}
-            credential.
-          </Typography>
           <Box
             sx={{
               display: 'flex',
-              justifyContent: 'center',
+              flexDirection: 'column',
+              justifyContent: 'space-around',
               alignItems: 'center',
+              minHeight: '830px',
             }}
           >
-            <fieldset
-              style={{
+            <Box>
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+                fontSize={48}
+                textAlign="center"
+              >
+                Proof of Credential created
+              </Typography>
+              <Typography
+                id="modal-modal-description"
+                sx={{ mt: 2, textAlign: 'center' }}
+                fontSize={16}
+              >
+                You have created the{' '}
+                <span style={{ color: '#D083FF' }}>
+                  Olympus Operations Working Group - Season 2
+                </span>{' '}
+                credential.
+              </Typography>
+            </Box>
+            <CredentialCard />
+            <Box
+              sx={{
                 display: 'flex',
-                border: '1px solid white',
-                borderRadius: '20px',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
-              <legend>Share</legend>
-              http://gtwy.xyz/olpmt
-              <AiOutlineCopy
-                size={24}
-                style={{ marginLeft: '10px', cursor: 'pointer' }}
-              />
-            </fieldset>
-            <Button
-              variant="outlined"
-              size="small"
-              sx={{ margin: '20px 0 0 20px' }}
-            >
-              Go to credentials
-            </Button>
+              <fieldset
+                style={{
+                  display: 'flex',
+                  border: '1px solid white',
+                  borderRadius: '20px',
+                }}
+              >
+                <legend>Share</legend>
+                http://gtwy.xyz/olpmt
+                <AiOutlineCopy
+                  size={24}
+                  style={{ marginLeft: '10px', cursor: 'pointer' }}
+                />
+              </fieldset>
+              <Button
+                variant="outlined"
+                size="small"
+                sx={{ margin: '20px 0 0 20px' }}
+              >
+                Go to credentials
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Modal>
