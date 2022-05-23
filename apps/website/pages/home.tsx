8727254f20f68da01593cf9ca6c2ffe4ca8a1b9a
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-05-23 15:48:31 -0300

    fix: missing user mock

--- apps/website/pages/home.tsx
+++ apps/website/pages/home.tsx
@@ -25,7 +25,7 @@ export async function getServerSideProps() {
   /* TODO: React-query will only work after auth is done */
   // await queryClient.prefetchQuery('home', () => gqlMethods.Users());
   const homeProps = await gqlMethodsServer.get_home({
-    id: 'eb06a881-6cdb-487f-bac3-eb771c2add2f',
+    id: 'e92ec36c-d003-46ac-ae3d-75f378070caa',
   });
 
   return {
