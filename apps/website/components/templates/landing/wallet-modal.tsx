00f94a52dd7eaa757c8df8520ae8ed4726cb0317
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-05-25 15:05:34 -0300

    feat: select wallet

--- apps/website/components/templates/landing/wallet-modal.tsx
+++ apps/website/components/templates/landing/wallet-modal.tsx
@@ -1,15 +1,23 @@
-import { useConnect, useAccount, useDisconnect } from 'wagmi';
+import { useEffect, useMemo, useState } from 'react';
+
+import { AnimatePresence } from 'framer-motion';
+import { useToggle } from 'react-use';
+import { useConnect } from 'wagmi';
 
 import {
   MetamaskIcon,
   CoinbaseWalletIcon,
   WalletConnectIcon,
 } from '@gateway/assets';
+import { MotionBox } from '@gateway/ui';
 
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import InboxIcon from '@mui/icons-material/Inbox';
+import CheckIcon from '@mui/icons-material/Check';
 import {
   Box,
+  Button,
+  CircularProgress,
+  DialogActions,
   DialogTitle,
   List,
   ListItem,
@@ -27,15 +35,71 @@ const icons = {
   injected: <AccountBalanceWalletIcon color="secondary" />,
 };
 
+function ConnectedWallet() {
+  const { activeConnector } = useConnect();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intv = setInterval(() => setCount((c) => c + 1), 1000);
+
+    return () => {
+      clearInterval(intv);
+    };
+  }, []);
+
+  return (
+    <Box>
+      <DialogTitle sx={{ textAlign: 'center' }}>
+        Connecting using {activeConnector.name}
+      </DialogTitle>
+      <Box
+        sx={{
+          height: 80,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <CircularProgress color="success" />
+      </Box>
+      <Box
+        sx={{
+          height: 36,
+          paddingX: 2,
+          paddingBottom: 4,
+          overflow: 'hidden',
+        }}
+      >
+        <Box sx={{ position: 'relative' }}>
+          <AnimatePresence>
+            <MotionBox
+              key={count}
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 100 }}
+              transition={{ ease: 'easeInOut' }}
+              sx={{ position: 'absolute', textAlign: 'center', width: '100%' }}
+            >
+              Test {count}
+            </MotionBox>
+          </AnimatePresence>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
 export function WalletModal() {
-  const {
-    activeConnector,
-    connect,
-    connectors,
-    error,
-    isConnecting,
-    pendingConnector,
-  } = useConnect();
+  const { connect, connectors, activeConnector, isConnected } = useConnect();
+
+  const [canConnect, toggleCanConnect] = useToggle(false);
+
+  const connectButtonLabel = useMemo(() => {
+    if (!isConnected && !activeConnector) return 'Connect to wallet first';
+    return `Connect using ${activeConnector.name}`;
+  }, [activeConnector, isConnected]);
+
+  if (canConnect) return <ConnectedWallet />;
 
   return (
     <>
@@ -55,13 +119,32 @@ export function WalletModal() {
       <List>
         {connectors.map((x) => (
           <ListItem key={x.id} disablePadding>
-            <ListItemButton disabled={!x.ready} onClick={() => connect(x)}>
+            <ListItemButton onClick={() => connect(x)}>
               <ListItemIcon>{icons[x.id]}</ListItemIcon>
               <ListItemText primary={x.name} />
+              <CheckIcon
+                color="primary"
+                sx={{
+                  ml: 4,
+                  visibility:
+                    x.id === activeConnector?.id ? 'visible' : 'hidden',
+                }}
+              />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
+      <DialogActions>
+        <Button
+          disabled={!isConnected}
+          onClick={toggleCanConnect}
+          variant="contained"
+          size="small"
+          fullWidth
+        >
+          {connectButtonLabel}
+        </Button>
+      </DialogActions>
       {/* {error && <div>{error.message}</div>} */}
     </>
   );
