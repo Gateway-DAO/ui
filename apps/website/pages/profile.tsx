29ec9a1874743566ef6cf22c121d30af85c21f02
fbenfraj <farouk.ben-fraj@outlook.com>
2022-05-30 14:45:49 +0200

    Add query to get user data and display on profile page

--- apps/website/pages/profile.tsx
+++ apps/website/pages/profile.tsx
@@ -1,10 +1,31 @@
+import { InferGetServerSidePropsType } from 'next';
+
+import { clearObject } from '@gateway/helpers';
+
 import { DashboardTemplate } from '../components/templates/dashboard';
 import { ProfileTemplate } from '../components/templates/profile';
+import { gqlMethodsServer } from '../services/api-server';
 
-export default function Profile() {
+export async function getServerSideProps() {
+  const user = (
+    await gqlMethodsServer.get_new_user({
+      id: '85cdb4aa-4ef7-4095-9414-119e1cdb0d1e',
+    })
+  )?.user;
+
+  return {
+    props: {
+      user: clearObject(user),
+    },
+  };
+}
+
+export default function Profile({
+  user,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <DashboardTemplate showExplore={false}>
-      <ProfileTemplate />
+      <ProfileTemplate user={user} />
     </DashboardTemplate>
   );
 }
