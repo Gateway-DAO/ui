3c0eab13ecef3aaeb4cb19a305683cde3937b48e
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-06-07 15:43:58 -0300

    feat: Explorer tabs (#16)
    * feat: added gitpods configs
    
    * feat: gate table view
    
    * feat: populating tabs
    
    * feat: migrated filtering and toggle view to hooks
    
    * feat: created daos table
    
    * feat: removed mocked daos
    
    * feat: removed max-height
    
    * feat: people tab
    
    * feat: change active tab
    
    * fix: add username on list
    
    * feat: follow position
    
    * feat: stretch item
    
    * chore: remove smooth scroll
    
    * feat: responsive action button placement
    
    * feat: explorer responsiveness

---
+++ apps/website/components/templates/explore/tabs/people-tab/people-tab.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material';
+
+import { Users } from '../../../../../services/graphql/types.generated';
+import { TableView } from './table-view';
+
+type Props = {
+  people: Users[];
+};
+
+export function PeopleTab({ people }: Props) {
+  return (
+    <Box sx={{ py: 4 }}>
+      <TableView people={people} />
+    </Box>
+  );
+}
---
+++ apps/website/components/templates/explore/tabs/use-property-filter.ts
@@ -0,0 +1,58 @@
+import { useCallback, useMemo, useState } from 'react';
+
+/**
+ * @typedef {Object} PropertyFilter
+ * @property {Object[]} filteredItems - the current items that are being filtered
+ * @property {string[]} availableFilters - List of available filters
+ * @property {string[]} selectedFilters - List of selected filters
+ * @property {method} toggleFilter - Toggles the filter on or off
+ */
+
+/**
+ * Generates a filtering functionality based on the given property of an object.
+ * It returns:
+ * - The available values that of the given property.
+ * - The current active filtered values
+ * - A filtered version of the given array
+ * -
+ * @param array - an array of items
+ * @param propertyOfItem - the property of the item that should be filtered
+ * @returns {PropertyFilter} -
+ */
+export function usePropertyFilter<Item>(
+  array: Item[],
+  propertyOfItem: keyof Item
+) {
+  const availableFilters = useMemo(
+    () =>
+      Array.from(
+        new Set<string>(
+          array.reduce(
+            (accumulator, item) =>
+              accumulator.concat(item[propertyOfItem] || []),
+            []
+          )
+        )
+      ),
+    [array, propertyOfItem]
+  );
+  const [selectedFilters, setActiveFilters] = useState<string[]>([]);
+  const toggleFilter = useCallback(
+    (name: string) =>
+      setActiveFilters((oldActiveFilters) =>
+        oldActiveFilters.includes(name)
+          ? oldActiveFilters.filter((filter) => filter !== name)
+          : [...oldActiveFilters, name]
+      ),
+    []
+  );
+
+  const filteredItems = useMemo(() => {
+    if (!selectedFilters.length) return array;
+    return array.filter((item) => {
+      const propertyArray = (item[propertyOfItem] as unknown as string[]) || [];
+      return propertyArray.some((value) => selectedFilters.includes(value));
+    });
+  }, [selectedFilters, array, propertyOfItem]);
+  return { filteredItems, availableFilters, selectedFilters, toggleFilter };
+}
