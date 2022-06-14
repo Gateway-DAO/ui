c94c43ceb95c53ef909f033a94a25c7e09361129
Nuno Carvalho <nunoormaker21@gmail.com>
2022-06-14 22:00:11 +0100

    Revert "Fixed Tweak on earn POC "

--- apps/website/constants/routes.ts
+++ apps/website/constants/routes.ts
@@ -1,7 +1,6 @@
 export const ROUTES = {
   LANDING: '/',
   EXPLORE: '/explore',
-  PROFILE: '/profile',
   NEW_USER: '/new-user',
   CREDENTIALS: '/credentials',
   CREDENTIALS_NEW: '/credentials/new',
