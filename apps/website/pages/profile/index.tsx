08589b561ee32d31cdce751f0084b33270054551
fbenfraj <farouk.ben-fraj@outlook.com>
2022-06-15 09:30:57 +0200

    Fix extra profile page mistake

--- apps/website/pages/profile/index.tsx
+++ apps/website/pages/profile/index.tsx
@@ -1,40 +1,40 @@
-import * as React from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { getSession } from 'next-auth/react';
 
-import { Container } from '@mui/material';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+import { DashboardTemplate } from '../../components/templates/dashboard';
+import { ProfileTemplate } from '../../components/templates/profile';
+import { gqlMethods } from '../../services/api';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
 
-export default function Profile() {
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true,
+      },
+      props: {
+        user: null,
+      },
+    };
+  }
+
+  const user = (await gqlMethods(session.user).get_new_user()).me;
+
+  return {
+    props: {
+      user,
+    },
+  };
+};
+
+export default function Profile({
+  user,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <Container maxWidth="xl" style={{ padding: '23px' }}>
-      <Box>
-        <div></div>
-        <div></div>
-      </Box>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
-    </Container>
+    <DashboardTemplate showExplore={false}>
+      <ProfileTemplate user={user} />
+    </DashboardTemplate>
   );
 }
