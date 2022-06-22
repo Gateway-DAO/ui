2d2fe5f277d42c0b783fcc87d8f69fd74c11caea
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-06-22 11:57:38 -0300

    feat: unified routes with i18n

---
+++ apps/website/constants/routes.js
@@ -0,0 +1,12 @@
+const ROUTES = {
+  LANDING: '/',
+  EXPLORE: '/app',
+  NEW_USER: '/app/new-user',
+  CREDENTIALS: '/app/credentials',
+  CREDENTIALS_NEW: '/app/credentials/new',
+  DAO_PROFILE: '/app/dao/[id]',
+};
+
+module.exports = {
+  ROUTES,
+};
