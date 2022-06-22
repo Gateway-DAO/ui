1f059400e6c1bac782cf149607d7da319545a9cf
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-06-22 12:08:29 -0300

    fix: moved back from /app

--- apps/website/constants/routes.js
+++ apps/website/constants/routes.js
@@ -1,10 +1,10 @@
 const ROUTES = {
   LANDING: '/',
-  EXPLORE: '/app',
-  NEW_USER: '/app/new-user',
-  CREDENTIALS: '/app/credentials',
-  CREDENTIALS_NEW: '/app/credentials/new',
-  DAO_PROFILE: '/app/dao/[id]',
+  EXPLORE: '/home',
+  NEW_USER: '/new-user',
+  CREDENTIALS: '/credentials',
+  CREDENTIALS_NEW: '/credentials/new',
+  DAO_PROFILE: '/dao/[id]',
 };
 
 module.exports = {
