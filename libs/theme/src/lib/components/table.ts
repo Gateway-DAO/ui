b4a6763239632fb0ec9e8fd62dd1a6b7746154a8
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-06-28 13:37:04 -0300

    feat: table head

--- libs/theme/src/lib/components/table.ts
+++ libs/theme/src/lib/components/table.ts
@@ -1,5 +1,17 @@
 import { Theme } from '@mui/material/styles';
 
+export const MuiTableHead: Extract<
+  Theme['components'],
+  object
+>['MuiTableHead'] = {
+  styleOverrides: {
+    root: ({ theme }) => ({
+      ...theme.typography.button,
+      color: theme.palette.text.secondary,
+    }),
+  },
+};
+
 export const MuiTableCell: Extract<
   Theme['components'],
   object
