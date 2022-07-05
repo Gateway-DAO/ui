c41320c3d67316fbc9b2e03f935454066c375e08
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-04 22:50:36 -0300

    feat: reusable gqlMethods

--- apps/website/types/user.ts
+++ apps/website/types/user.ts
@@ -1,6 +1,9 @@
+import { PartialDeep } from 'type-fest';
+
 import { MeQuery, LoginMutation } from '../services/graphql/types.generated';
 
-export type SessionUser = Omit<
-  MeQuery['me'] & LoginMutation['login'],
-  '__typename'
-> & { __typename?: any };
+export type SessionUser = PartialDeep<
+  Omit<MeQuery['me'] & LoginMutation['login'], '__typename'> & {
+    __typename?: any;
+  }
+>;
