ffc76db120b00fe6730f0380750694df27ae9532
fbenfraj <farouk.ben-fraj@outlook.com>
2022-07-05 12:59:50 +0200

    Update validation schema for create gate

--- apps/website/components/templates/create-gate/schema.ts
+++ apps/website/components/templates/create-gate/schema.ts
@@ -1,12 +1,27 @@
 import { object, string, array, SchemaOf } from 'yup';
 
+// Files & Links
+type FileTaskTypes = {
+  title: string;
+  description: string;
+  files: FileTypes[];
+};
+
+type FileTypes = {
+  title: string;
+  description: string;
+  link: string;
+};
+
+// Create Gate
 export type CreateGateTypes = {
   title: string;
   categories: string[];
   description: string;
   image: string;
-  created_by: string;
   skills: string[];
+  created_by: string;
+  tasks: FileTaskTypes[];
 };
 
 export const createGateSchema: SchemaOf<CreateGateTypes> = object({
@@ -14,6 +29,23 @@ export const createGateSchema: SchemaOf<CreateGateTypes> = object({
   categories: array().of(string()).defined(),
   description: string().min(2).defined(),
   image: string().min(2).defined(),
-  created_by: string().min(2).defined(),
   skills: array().of(string()).defined(),
+  created_by: string().min(2).defined(),
+  tasks: array()
+    .of(
+      object({
+        title: string().min(2).defined(),
+        description: string().min(2).defined(),
+        files: array()
+          .of(
+            object({
+              title: string().min(2).defined(),
+              description: string().min(2).defined(),
+              link: string().min(2).defined(),
+            })
+          )
+          .defined(),
+      })
+    )
+    .defined(),
 });
