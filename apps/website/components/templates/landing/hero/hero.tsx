62f352e50b835f36ee2af4072b0e050fa55d3296
Rafa Gomes <rafaelgomes.web@gmail.com>
2022-07-06 12:48:46 -0300

    adjusting image in hero

--- apps/website/components/templates/landing/hero/hero.tsx
+++ apps/website/components/templates/landing/hero/hero.tsx
@@ -1,11 +1,13 @@
 import dynamic from 'next/dynamic';
-import Image from 'next/image';
 
+import { TOKENS } from '@gateway/theme';
 import { MotionBox } from '@gateway/ui';
 
-import { Box, Stack, Typography } from '@mui/material';
-import { alpha } from '@mui/system';
+import { ArrowDownward } from '@mui/icons-material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 
+import BackgroundImage from '../../../../public/images/hero-background.png';
+import { HeroBackground } from './styles';
 import { HeroProps } from './types';
 const Title = dynamic(() => import('../title'));
 
@@ -16,93 +18,85 @@ export function Hero({
   titleDescription,
 }: HeroProps): JSX.Element {
   return (
-    <Stack direction="column" sx={{ flex: 1, width: '100%' }}>
-      <MotionBox
-        sx={{
-          display: 'flex',
-          position: 'absolute',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          zIndex: 2,
-          top: (theme) => theme.spacing(14),
-        }}
-        initial={{ translateY: 20, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
-        transition={{
-          ease: 'easeOut',
-          duration: 0.75,
-          opacity: { duration: 0.5 },
-        }}
-      >
-        <Title>
-          <Typography
-            component="span"
-            variant="h1"
-            sx={(theme) => ({
-              color: theme.palette.primary.main,
-              display: 'block',
-            })}
-          >
-            {title}
-          </Typography>
-          {subtitle}
-        </Title>
-        <Typography
-          component="h2"
-          variant="subtitle1"
-          sx={(theme) => ({
-            color: theme.palette.secondary.main,
-            marginTop: '34px',
-            maxWidth: '338px',
-          })}
-        >
-          {titleDescription}
-        </Typography>
-        {enterButton}
-      </MotionBox>
-      <Box
-        sx={{
-          flex: 1,
-          position: 'relative',
-          overflow: 'hidden',
-          ':before': {
-            background: (theme) =>
-              `linear-gradient(to bottom, ${
-                theme.palette.background.default
-              } 0%, ${alpha(theme.palette.background.default, 0)} 35%)`,
-            content: '""',
-            position: 'absolute',
-            inset: 0,
-            zIndex: 1,
-          },
-        }}
+    <Box component="section" sx={{ height: '100%', width: '100%' }}>
+      <Stack
+        direction="column"
+        sx={{ flex: 1, width: '100%', px: TOKENS.CONTAINER_PX }}
       >
         <MotionBox
-          initial={{ scale: 1.2, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
+          sx={{
+            display: 'flex',
+            position: 'absolute',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            zIndex: 2,
+            top: (theme) => theme.spacing(14),
+          }}
+          initial={{ translateY: 20, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
           transition={{
             ease: 'easeOut',
-            duration: 1,
+            duration: 0.75,
             opacity: { duration: 0.5 },
           }}
-          sx={{
-            width: '61%',
-            height: '86%',
-            position: 'absolute',
-            bottom: '0',
-            right: '0',
-          }}
         >
-          <Image
-            src="/images/hero-background.png"
-            layout="fill"
-            objectFit="cover"
-            width={881}
-            height={881}
-            alt="Gateway's background image"
-          />
+          <Title>
+            <Typography
+              component="span"
+              sx={(theme) => ({
+                color: theme.palette.primary.main,
+                display: 'block',
+                fontSize: 'inherit',
+                fontWeight: 'inherit',
+                lineHeight: 'inherit',
+              })}
+            >
+              {title}
+            </Typography>
+            {subtitle}
+          </Title>
+          <Typography
+            component="h2"
+            variant="subtitle1"
+            sx={(theme) => ({
+              color: theme.palette.secondary.main,
+              marginTop: '34px',
+              maxWidth: '338px',
+            })}
+          >
+            {titleDescription}
+          </Typography>
+          {enterButton}
+          <Button
+            variant="outlined"
+            sx={{ borderRadius: '50%', padding: '20px' }}
+          >
+            <ArrowDownward />
+          </Button>
         </MotionBox>
-      </Box>
-    </Stack>
+      </Stack>
+      <MotionBox
+        initial={{ scale: 1.2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          ease: 'easeOut',
+          duration: 1,
+          opacity: { duration: 0.5 },
+        }}
+        sx={{
+          width: '61%',
+          height: '86%',
+          position: 'absolute',
+          bottom: '0',
+          right: '0',
+        }}
+      >
+        <HeroBackground
+          src={BackgroundImage}
+          layout="fixed"
+          alt="Gateway's background with people joining the network"
+        />
+      </MotionBox>
+    </Box>
   );
 }
---
+++ apps/website/components/templates/landing/hero/styles.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+import styled from '@emotion/styled';
+
+export const HeroBackground = styled(Image)`
+  /* position: absolute;
+  bottom: 0;
+  right: 0; */
+`;
