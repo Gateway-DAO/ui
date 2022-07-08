a4dff67375a9eb9c8bc7ee9232da7ea8eae785f5
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-08 11:33:47 -0300

    feat: error and responsiveness

--- apps/website/components/molecules/form/social-links/social-link.tsx
+++ apps/website/components/molecules/form/social-links/social-link.tsx
@@ -14,7 +14,9 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Stack,
   TextField,
+  Typography,
 } from '@mui/material';
 
 
@@ -47,8 +49,15 @@ export function SocialLink<TFormSchema extends FieldValues = FieldValues>({
   const { t } = useTranslation();
 
   return (
-    <Card sx={{ display: 'flex', flexFlow: 'row', gap: 2, p: 2 }}>
-      <FormControl sx={{ flex: 0.25 }}>
+    <Card sx={{ display: 'flex', flexFlow: 'column', gap: 2, p: 2 }}>
+      <Stack sx={{flexFlow: "row", gap: 2, flexWrap: {
+        xs: "wrap",
+        md: "nowrap"
+      } }}>
+      <FormControl sx={{ flex: {
+        xs: 1,
+        md: 0.5
+      }}}>
         <InputLabel id={`${name}.network`}>
           {t('common:fields.platform')}
         </InputLabel>
@@ -58,6 +67,7 @@ export function SocialLink<TFormSchema extends FieldValues = FieldValues>({
           defaultValue=""
           {...networkField.field}
           value={networkField.field.value ?? ''}
+          error={!!networkField.fieldState.error}
         >
           {networks.map((network) => (
             <MenuItem key={network.value} value={network.value}>
@@ -69,15 +79,33 @@ export function SocialLink<TFormSchema extends FieldValues = FieldValues>({
       <TextField
         label={t('common:fields.link')}
         {...linkField.field}
-        sx={{ flex: 1 }}
+        sx={{ flex: 1, flexBasis: {
+          xs: "100%",
+          md: "unset"
+        }, order: {
+          xs: 2,
+          md: 1
+        } }}
+        error={!!linkField.fieldState.error}
       />
       <IconButton
         aria-label="Delete this line"
         onClick={onDelete}
-        sx={{ alignSelf: 'center' }}
+        sx={{ alignSelf: 'center', order: {
+          xs: 1,
+          md: 2,
+        } }}
       >
         <Delete />
       </IconButton>
+      </Stack>
+      {(!!networkField.fieldState.error) && <Typography color="error">
+        {networkField.fieldState.error.message}
+        </Typography>}
+        {(!!linkField.fieldState.error) && <Typography color="error">
+      {linkField.fieldState.error.message}
+        </Typography>}
+
     </Card>
   );
 }
