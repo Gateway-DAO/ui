77f783d1ec211a81d3e7951d555a4db6bdcc30a2
Ashish Goyal <>
2022-07-12 14:22:14 +0530

    Edit Early Profile

---
+++ apps/website/pages/private_profile/edit_early/index.tsx
@@ -0,0 +1,8 @@
+import { EditProfile } from "../../../components/templates/private-profile/edit-profile-early/EditProfile";
+export default function edit() {
+  return (
+    <>
+      <EditProfile />
+    </>
+  );
+}
