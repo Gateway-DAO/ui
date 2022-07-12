75550cce25a3f37d50f075d5cf11927eb65d615f
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-12 15:51:43 -0300

    fix: definition type

--- libs/theme/src/index.ts
+++ libs/theme/src/index.ts
@@ -1,4 +1,5 @@
 export * from './ui/theme-provider/theme-provider';
 export * from './lib/theme';
+export * from './lib/config/pallete';
 export * as TOKENS from './ui/style-tokens/style-tokens';
 export * from './types';
--- libs/theme/src/lib/theme.ts
+++ libs/theme/src/lib/theme.ts
@@ -4,16 +4,6 @@ import * as components from './components';
 import palette from './config/palette';
 import typography from './config/typography';
 
-declare module '@mui/material/styles' {
-  interface TypeBackground {
-    light: string;
-    elevated: string;
-  }
-  interface PaletteColorOptions {
-    elevated: string;
-  }
-}
-
 export const theme = createTheme({
   palette,
   typography,
