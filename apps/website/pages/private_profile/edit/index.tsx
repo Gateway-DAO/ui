a81b6244d859f0e940e6e9c7fca22dc10825b741
Ashish Goyal <>
2022-07-12 14:22:39 +0530

    Edit Profile - Experience card

---
+++ apps/website/pages/private_profile/edit/index.tsx
@@ -0,0 +1,8 @@
+import { EditProfile } from "../../../components/templates/private-profile/edit-profile/EditProfile";
+export default function edit() {
+  return (
+    <>
+      <EditProfile />
+    </>
+  );
+}
