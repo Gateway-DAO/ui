880b8e5743a9d1d9524a27033c5e542f32354c56
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-13 15:52:16 -0300

    chore: test page

---
+++ apps/website/pages/test.tsx
@@ -0,0 +1,32 @@
+import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
+
+import { useAuth } from '../providers/auth';
+
+export default function Test() {
+  const { me, gqlAuthMethods, onUpdateMe } = useAuth();
+
+  const { register, handleSubmit } = useForm();
+
+  const mutation = useMutation(gqlAuthMethods.gates_tab, {
+    onSuccess(data) {
+      console.log('Data succ', data);
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data.token);
+    onUpdateMe((oldMe) => ({ ...oldMe, token: data.token }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <textarea {...register('token')} />
+      <button type="submit">Submit</button>
+      <br />
+      <button type="button" onClick={() => mutation.mutate({})}>
+        Test
+      </button>
+    </form>
+  );
+}
