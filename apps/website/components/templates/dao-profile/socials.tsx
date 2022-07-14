9087bc8027f0666802aa7c2848859256ec08a98f
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-14 15:24:21 -0300

    fix: socials link

--- apps/website/components/templates/dao-profile/socials.tsx
+++ apps/website/components/templates/dao-profile/socials.tsx
@@ -57,11 +57,16 @@ export function Socials({ dao, children }: PropsWithChildren<Props>) {
           </Avatar>
         </IconButton>
         {dao.socials?.map((social) => (
-          <Link href={social.url} key={social.url + social.network} passHref>
-            <Avatar component="a" title={social.network}>
-              <SocialIcon icon={social.network as Network} />
-            </Avatar>
-          </Link>
+          <Avatar
+            component="a"
+            title={social.network}
+            href={social.url}
+            key={social.url + social.network}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SocialIcon icon={social.network as Network} />
+          </Avatar>
         ))}
       </Stack>
       <Snackbar
