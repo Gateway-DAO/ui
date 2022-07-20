2d3f8701ac5d31b81255ed35ff336bf7e3b7e165
fbenfraj <farouk.ben-fraj@outlook.com>
2022-07-20 18:24:05 +0200

    Add token hold task content

--- apps/website/components/organisms/gates/view/tasks/content/token_hold.tsx
+++ apps/website/components/organisms/gates/view/tasks/content/token_hold.tsx
@@ -1,7 +1,25 @@
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
-const TokenHoldContent = () => {
-  return <Typography>TokenHoldContent</Typography>;
+const TokenHoldContent = ({ data }) => {
+  const { chain, token_address, quantity } = data;
+  const uppercaseChain = chain.charAt(0).toUpperCase() + chain.slice(1);
+
+  return (
+    <Stack marginTop={5}>
+      <Typography variant="subtitle1" fontWeight={'bold'}>
+        {uppercaseChain}
+      </Typography>
+      <Typography variant="caption">Chain</Typography>
+      <Typography variant="subtitle1" fontWeight={'bold'} marginTop={2}>
+        {token_address}
+      </Typography>
+      <Typography variant="caption">Token address</Typography>
+      <Typography variant="subtitle1" fontWeight={'bold'} marginTop={2}>
+        {quantity}
+      </Typography>
+      <Typography variant="caption">Quantity</Typography>
+    </Stack>
+  );
 };
 
 export default TokenHoldContent;
