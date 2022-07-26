f11d18360c9a8f4f9def400ea978474c623ffebe
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-07-26 17:31:58 -0300

    feat: refetch on usage

--- apps/website/services/query-client.ts
+++ apps/website/services/query-client.ts
@@ -6,8 +6,8 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: true,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: true,
     },
   },
 });
