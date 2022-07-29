856bba5a65ffe3912d0833284dc13b6e4b496c11
fbenfraj <farouk.ben-fraj@outlook.com>
2022-07-29 18:55:20 +0200

    Fix: Uncomment new credential page to fix build

--- apps/website/pages/credential/new.tsx
+++ apps/website/pages/credential/new.tsx
@@ -1,9 +1,8 @@
 // Commented for future usage
-
 // import { NewCredentialTemplate } from '../../components/templates/new-credential';
 
-// export default function NewCredential() {
-//   return <NewCredentialTemplate />;
-// }
+export default function NewCredential() {
+  // return <NewCredentialTemplate />;
+}
 
-// NewCredential.auth = true;
+NewCredential.auth = true;
