30d257d5c5d59dd4918470f63d316e7fd3e65287
Manish Rawat <62084296+R11manish@users.noreply.github.com>
2022-08-01 18:47:27 +0530

    fix : Layout alignment between details and tasks (#80)

--- apps/website/components/templates/create-gate/create-gate.tsx
+++ apps/website/components/templates/create-gate/create-gate.tsx
@@ -175,6 +175,7 @@ export function CreateGateTemplate() {
               direction="column"
               sx={{
                 margin: 'auto',
+                marginLeft : 8,
                 maxWidth: { xs: '100%', md: '100%', lg: '80%' },
               }}
             >
