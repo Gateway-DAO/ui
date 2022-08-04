2f85bfaf7417a630e98e599eaab97ff251b8f5b4
Nuno Carvalho <nunoormaker21@gmail.com>
2022-08-04 19:49:52 +0100

    hotfix: fixed problem with Smartlook

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -52,16 +52,19 @@ function CustomApp({ Component, pageProps: { ...pageProps } }: AppProps) {
           </QueryClientProvider>
         </ThemeProvider>
       </WagmiConfig>
-      <Script strategy="beforeInteractive">
-        {`
-        window.smartlook||(function(d) {
-          var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-          var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-          c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js%27;h.appendChild(c);
-          })(document);
-          smartlook('init', '${process.env.NEXT_PUBLIC_SMARTLOOK_KEY}', { region: 'eu' });
-        `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', '${process.env.NEXT_PUBLIC_SMARTLOOK_KEY}', { region: 'eu' });
+            `,
+        }}
+      />
     </>
   );
 }
