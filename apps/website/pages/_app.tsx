2a86c62f4e85f51f0be99dfaf9776b55ef5487a6
Nuno Carvalho <nunoormaker21@gmail.com>
2022-08-04 19:42:06 +0100

    feat: added Smartlook

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/inline-script-id */
 import NextProgress from 'next-progress';
 import { AppProps as NextAppProps } from 'next/app';
 import Head from 'next/head';
@@ -19,6 +20,8 @@ import '../styles/next.css';
 import { AuthProvider } from '../providers/auth';
 import { queryClient } from '../services/query-client';
 import { web3client } from '../services/web3/client';
+
+import Script from 'next/script';
 
 type AppProps = NextAppProps & {
   Component: NextAppProps['Component'] & { auth?: boolean };
@@ -49,6 +52,16 @@ function CustomApp({ Component, pageProps: { ...pageProps } }: AppProps) {
           </QueryClientProvider>
         </ThemeProvider>
       </WagmiConfig>
+      <Script strategy="beforeInteractive">
+        {`
+        window.smartlook||(function(d) {
+          var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+          var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+          c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js%27;h.appendChild(c);
+          })(document);
+          smartlook('init', '${process.env.NEXT_PUBLIC_SMARTLOOK_KEY}', { region: 'eu' });
+        `}
+      </Script>
     </>
   );
 }
