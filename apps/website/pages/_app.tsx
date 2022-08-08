6cd6f6b75e4aab56d9839a6a84513dbd42881e29
Farouk Ben Fraj <farouk.ben-fraj@outlook.com>
2022-08-08 13:55:07 +0100

    fix(global): change linear progress bar from next to materialui (#91)

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/inline-script-id */
-import NextProgress from 'next-progress';
 import { AppProps as NextAppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
 
 import { Hydrate, QueryClientProvider } from 'react-query';
 import { WagmiConfig } from 'wagmi';
@@ -9,27 +11,43 @@ import { WagmiConfig } from 'wagmi';
 import { ThemeProvider } from '@gateway/theme';
 
 import { useTheme } from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
 
 import { SEOSocial, SEOFavicon } from '../components/atoms/seo';
 import { NavStateProvider } from '../hooks/use-nav';
 import { usePersistLocale } from '../hooks/usePersistLocale';
-
-import '../components/atoms/global-dependencies';
-
-import '../styles/next.css';
 import { AuthProvider } from '../providers/auth';
 import { queryClient } from '../services/query-client';
 import { web3client } from '../services/web3/client';
 
-import Script from 'next/script';
+import '../components/atoms/global-dependencies';
+import '../styles/next.css';
 
 type AppProps = NextAppProps & {
   Component: NextAppProps['Component'] & { auth?: boolean };
 };
 
 function CustomApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  usePersistLocale();
   const theme = useTheme();
+  const router = useRouter();
+  usePersistLocale();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      setIsLoading(true);
+    });
+
+    router.events.on('routeChangeComplete', () => {
+      setIsLoading(false);
+    });
+
+    router.events.on('routeChangeError', () => {
+      setIsLoading(false);
+    });
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -38,7 +56,16 @@ function CustomApp({ Component, pageProps: { ...pageProps } }: AppProps) {
         <SEOFavicon />
         <SEOSocial />
       </Head>
-      <NextProgress color={theme.palette.primary.main} />
+      {isLoading && (
+        <LinearProgress
+          sx={{
+            backgroundColor: `${theme.palette.secondary.main}`,
+            '& .MuiLinearProgress-bar': {
+              backgroundColor: `${theme.palette.secondary.dark}`,
+            },
+          }}
+        />
+      )}
       <WagmiConfig client={web3client}>
         <ThemeProvider>
           <QueryClientProvider client={queryClient}>
