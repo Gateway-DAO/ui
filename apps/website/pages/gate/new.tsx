19c11970d6763a80ea4f7b4b847324ac17118b8c
fbenfraj <farouk.ben-fraj@outlook.com>
2022-08-12 15:54:40 +0200

    feat(draft-gate): fetch draft gate data and pass it as props

--- apps/website/pages/gate/new.tsx
+++ apps/website/pages/gate/new.tsx
@@ -3,26 +3,44 @@ import { useEffect } from 'react';
 
 import { CreateGateTemplate } from '../../components/templates/create-gate';
 import { ROUTES } from '../../constants/routes';
+import { gqlAnonMethods } from '../../services/api';
 
-export default function CreateGate() {
+export default function CreateGate({ gateProps }) {
   const router = useRouter();
 
   const {
     isReady,
-    query: { dao },
+    query: { dao: daoId },
   } = router;
 
   useEffect(() => {
-    if (isReady && !dao) {
+    if (isReady && !daoId) {
       router.replace(ROUTES.EXPLORE);
     }
-  }, [dao, isReady]);
+  }, [daoId, isReady, router]);
 
-  if (!dao) {
+  if (!daoId) {
     return null;
   }
 
-  return <CreateGateTemplate />;
+  return <CreateGateTemplate gateData={gateProps} />;
+}
+
+export async function getServerSideProps({ query }) {
+  const { gate: gateId } = query;
+  let gateProps = { gates_by_pk: {} };
+
+  if (gateId) {
+    gateProps = await gqlAnonMethods.gate({
+      id: gateId,
+    });
+  }
+
+  return {
+    props: {
+      gateProps: gateProps.gates_by_pk,
+    },
+  };
 }
 
 CreateGate.auth = true;
