47870887498d3b20b867732220c38bd5ab955696
R11manish <github.jmtss@gmail.com>
2022-08-16 16:38:58 +0530

    styles : fix padding responsiveness issue

--- apps/website/components/templates/credential/credential.tsx
+++ apps/website/components/templates/credential/credential.tsx
@@ -25,7 +25,14 @@ type Props = {
 export function CredentialTemplate({ credential }: Props) {
   return (
     <Grid container height="100%">
-      <Grid item xs={12} md={5} p={(theme) => theme.spacing(7)}>
+      <Grid
+        item
+        xs={12}
+        md={5}
+        sx={(theme) => ({
+          p: { xs: theme.spacing(3), md: theme.spacing(7) },
+        })}
+      >
         {/* DAO info */}
         <Stack
           direction="row"
