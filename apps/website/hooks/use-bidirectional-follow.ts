56da8193ff39b48e3ca9365835499de8217fcb9c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-09-01 11:24:44 -0300

    feat: refresh and onToggleFollow

--- apps/website/hooks/use-bidirectional-follow.ts
+++ apps/website/hooks/use-bidirectional-follow.ts
@@ -3,16 +3,29 @@ import { BiConnectionType } from '@cyberlab/cyberconnect';
 import { useCyberConnect } from '../providers/cyberconnect';
 
 export const useBidirectionFollow = () => {
-  const { cyberConnect } = useCyberConnect();
+  const { cyberConnect, onRefetch } = useCyberConnect();
 
-  const onFollow = (wallet: string) =>
-    cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.INIT);
-  const onAccept = (wallet: string) =>
-    cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.ACCEPT);
-  const onUnfollow = (wallet: string) =>
-    cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.TERMINATE);
+  const onFollow = async (wallet: string) => {
+    await cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.INIT);
+    return onRefetch();
+  };
+  const onAccept = async (wallet: string) => {
+    await cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.ACCEPT);
+    return onRefetch();
+  };
+  const onReject = async (wallet: string) => {
+    await cyberConnect?.bidirectionalConnect(wallet, BiConnectionType.REJECT);
+    return onRefetch();
+  };
+  const onUnfollow = async (wallet: string) => {
+    await cyberConnect?.bidirectionalConnect(
+      wallet,
+      BiConnectionType.TERMINATE
+    );
+    return onRefetch();
+  };
 
   const onToggleFollow = (wallet: string, isFollowing: boolean) =>
     isFollowing ? onUnfollow(wallet) : onFollow(wallet);
-  return { onFollow, onUnfollow, onToggleFollow };
+  return { onFollow, onAccept, onReject, onUnfollow, onToggleFollow };
 };
