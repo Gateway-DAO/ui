d632dd28563a67d7e43572e44a6a13118fbf2fa4
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-09-05 15:51:09 -0300

    chore: cleanup

--- apps/website/components/atoms/follow-button-user/pending-sent.tsx
+++ apps/website/components/atoms/follow-button-user/pending-sent.tsx
@@ -1,38 +1,24 @@
 import useTranslation from 'next-translate/useTranslation';
 
-import { useMutation } from 'react-query';
-
 import { useMenu } from '@gateway/ui';
 
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button } from '@mui/material';
 
-import { useBidirectionFollow } from '../../../hooks/use-bidirectional-follow';
-import { LoadingButton } from '../loading-button';
 import { FollowButtonProps } from './type';
 
-export function FriendSentPendingButton({
-  wallet,
-  ...props
-}: FollowButtonProps) {
+export function FriendSentPendingButton(props: FollowButtonProps) {
   const menu = useMenu();
   const { t } = useTranslation('common');
-  // const { onUnfollow } = useBidirectionFollow();
-  // const unfollowMutation = useMutation(() => onUnfollow(wallet));
+
   return (
-    <>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={menu.onOpen}
-        disabled
-        // isLoading={unfollowMutation.isLoading}
-        {...props}
-      >
-        {t('actions.pending')}
-      </Button>
-      {/* <Menu anchorEl={menu.element} open={menu.isOpen} onClose={menu.onClose}>
-        <MenuItem onClick={() => { unfollowMutation.mutate(); menu.onClose(); }}>{t('actions.withdraw')}</MenuItem>
-      </Menu> */}
-    </>
+    <Button
+      variant="outlined"
+      color="primary"
+      onClick={menu.onOpen}
+      disabled
+      {...props}
+    >
+      {t('actions.pending')}
+    </Button>
   );
 }
