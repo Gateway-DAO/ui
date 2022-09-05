3b2629bb0ee70bb9d70ee0e40ea8126f92c4bf95
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-09-05 15:23:14 -0300

    feat: user list with link

--- apps/website/components/templates/profile/connections/users.tsx
+++ apps/website/components/templates/profile/connections/users.tsx
@@ -1,9 +1,17 @@
+import Link from 'next/link';
 import { Fragment } from 'react';
 
 import { PartialDeep } from 'type-fest';
 
-import { Divider, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import {
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  Link as MUILink,
+  Stack,
+} from '@mui/material';
 
+import { ROUTES } from '../../../../constants/routes';
 import { useAuth } from '../../../../providers/auth';
 import { Users } from '../../../../services/graphql/types.generated';
 import { AvatarFile } from '../../../atoms/avatar-file';
@@ -17,23 +25,52 @@ export function UsersList({ users }: Props) {
   const { me } = useAuth();
   return (
     <>
-      {users.map((user, index) => (
-        <Fragment key={index}>
-          <ListItem
-            secondaryAction={
-              me?.wallet !== user.wallet ? (
-                <FollowButtonUser wallet={user.wallet} variant="outlined" />
-              ) : undefined
-            }
-          >
-            <ListItemAvatar>
-              <AvatarFile file={user.picture} />
-            </ListItemAvatar>
-            <ListItemText primary={user.name} secondary={`@${user.username}`} />
-          </ListItem>
-          {index !== users.length - 1 && <Divider component="li" />}
-        </Fragment>
-      ))}
+      {users.map((user, index) => {
+        const url = ROUTES.PROFILE.replace('[username]', user.username);
+        return (
+          <Fragment key={index}>
+            <ListItem
+              secondaryAction={
+                me?.wallet !== user.wallet ? (
+                  <FollowButtonUser wallet={user.wallet} variant="outlined" />
+                ) : undefined
+              }
+            >
+              <ListItemAvatar>
+                <Link passHref href={url}>
+                  <AvatarFile
+                    component="a"
+                    file={user.picture}
+                    target="_blank"
+                  />
+                </Link>
+              </ListItemAvatar>
+              <Stack direction="column">
+                <Link passHref href={url}>
+                  <MUILink
+                    color="text.primary"
+                    underline="hover"
+                    target="_blank"
+                  >
+                    {user.name}
+                  </MUILink>
+                </Link>
+                <Link passHref href={url}>
+                  <MUILink
+                    variant="body2"
+                    color="text.secondary"
+                    underline="hover"
+                    target="_blank"
+                  >
+                    {`@${user.username}`}
+                  </MUILink>
+                </Link>
+              </Stack>
+            </ListItem>
+            {index !== users.length - 1 && <Divider component="li" />}
+          </Fragment>
+        );
+      })}
     </>
   );
 }
