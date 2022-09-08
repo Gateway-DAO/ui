d11d5a7170abdf0bc100a222957e00d711767919
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-09-08 16:09:49 -0300

    feat: infinite query people tab

--- apps/website/components/templates/explore/tabs/people-tab/people-tab.tsx
+++ apps/website/components/templates/explore/tabs/people-tab/people-tab.tsx
@@ -1,14 +1,34 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 
-import { Box, CircularProgress } from '@mui/material';
+import { Box, Button, CircularProgress } from '@mui/material';
 
 import { gqlAnonMethods } from '../../../../../services/api';
 import { TableView } from './table-view';
 
+const offset = 0;
+
 export function PeopleTab() {
-  const { data: people, isLoading } = useQuery('people-tab', async () => {
-    return (await gqlAnonMethods.people_tab()).people;
-  });
+  const {
+    data,
+    isLoading,
+    isFetching,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ['people-tab'],
+    ({ pageParam = offset }) =>
+      gqlAnonMethods.people_tab({ offset: pageParam }),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.people.length < 15) return undefined;
+        console.log(pages.length * 15);
+        return pages.length * 15;
+      },
+    }
+  );
+
+  const people = data?.pages?.flatMap((page) => page.people) ?? [];
   return (
     <Box sx={{ py: 4 }}>
       {isLoading ? (
@@ -22,7 +42,32 @@ export function PeopleTab() {
           <CircularProgress />
         </Box>
       ) : (
-        <TableView people={people ?? []} />
+        <>
+          <TableView people={people ?? []} />
+          {isFetchingNextPage && (
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                py: 5,
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          )}
+          {hasNextPage && (
+            <Button
+              type="button"
+              disabled={isFetchingNextPage}
+              onClick={() => fetchNextPage()}
+              fullWidth
+              sx={{ my: 4 }}
+            >
+              Load More
+            </Button>
+          )}
+        </>
       )}
     </Box>
   );
