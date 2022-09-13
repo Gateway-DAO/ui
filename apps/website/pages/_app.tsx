3deee53b726db77325c47706bde3e32a66fb19ee
Nuno Carvalho <nunoormaker21@gmail.com>
2022-09-13 13:07:00 +0100

    fix: reverted changes to progress bar

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/inline-script-id */
-import NextProgress from 'next-progress';
 import { AppProps as NextAppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
+import NextNProgress from 'nextjs-progressbar';
 
 import { WagmiConfig } from 'wagmi';
 
@@ -42,9 +42,9 @@ function CustomApp({ Component, pageProps: { ...pageProps } }: AppProps) {
         <SEOFavicon />
         <SEOSocial />
       </Head>
-      <NextProgress
-        height="4px"
-        color={theme.palette.primary.main}
+      <NextNProgress
+        height={4}
+        color={'#9A53FF'}
         options={{ showSpinner: false }}
       />
       <WagmiConfig client={web3client}>
