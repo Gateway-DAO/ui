ad4a32ad42cc7440df520a08d4cc352af0a1b53c
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-09-20 12:05:37 -0300

    feat: refresh token

--- apps/website/pages/api/auth/[...nextauth].ts
+++ apps/website/pages/api/auth/[...nextauth].ts
@@ -1,78 +1,105 @@
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { gqlAnonMethods } from '../../../services/api';
+import { SessionToken } from '../../../types/user';
+
+const callLogin = async (
+  signature: string,
+  wallet: string
+): Promise<SessionToken> => {
+  try {
+    const res = await gqlAnonMethods.login({
+      signature,
+      wallet,
+    });
+
+    const { error } = (res as any) ?? {};
+
+    if (error || !res.login) {
+      throw error;
+    }
+
+    const { __typename, ...token } = res.login;
+
+    return {
+      ...token,
+      expiry: Date.parse(token.expiry),
+    };
+  } catch (e) {
+    return null;
+  }
+};
+const callRefresh = async (refresh_token: string): Promise<SessionToken> => {
+  try {
+    const res = await gqlAnonMethods.refresh({
+      refresh_token,
+    });
+
+    const { error } = (res as any) ?? {};
+
+    if (error || !res.refresh) {
+      throw error;
+    }
+
+    const { __typename, ...token } = res.refresh;
+
+    return {
+      ...token,
+      expiry: Date.parse(token.expiry),
+    };
+  } catch (e) {
+    return null;
+  }
+};
+
+const providers = [
+  CredentialsProvider({
+    name: 'ethereum',
+    credentials: {
+      wallet: {
+        label: 'wallet',
+        type: 'text',
+        placeholder: '0x0',
+      },
+      signature: {
+        label: 'signature',
+        type: 'text',
+        placeholder: '0x0',
+      },
+    },
+    async authorize(credentials) {
+      return callLogin(credentials.signature, credentials.wallet);
+    },
+  }),
+];
+
+export const config: NextAuthOptions = {
+  providers,
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      // We're retrieving the token from the provider
+      if (user) {
+        token = user;
+      }
+
+      if (token.expiry < Date.now()) {
+        return callRefresh(token.refresh_token);
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      session.token = token.token as string;
+      session.refresh_token = token.refresh_token as string;
+      return session;
+    },
+  },
+};
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export default async function auth(req, res) {
-  const providers = [
-    CredentialsProvider({
-      name: 'ethereum',
-      credentials: {
-        wallet: {
-          label: 'wallet',
-          type: 'text',
-          placeholder: '0x0',
-        },
-        signature: {
-          label: 'signature',
-          type: 'text',
-          placeholder: '0x0',
-        },
-      },
-      async authorize(credentials) {
-        try {
-          const res = await gqlAnonMethods.login({
-            signature: credentials.signature,
-            wallet: credentials.wallet,
-          });
-
-          const { error } = (res as any) ?? {};
-
-          if (error || !res.login) {
-            throw error;
-          }
-
-          const { __typename, ...token } = res.login;
-
-          return {
-            ...token,
-          };
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
-  ];
-
-  // const isDefaultSigninPage =
-  //   req.method === 'GET' && req.query.nextauth.includes('signin');
-
-  // // Hides Sign-In with Ethereum from default sign page
-  // if (isDefaultSigninPage) {
-  //   providers.pop();
-  // }
-
-  return await NextAuth(req, res, {
-    // https://next-auth.js.org/configuration/providers/oauth
-    providers,
-    session: {
-      strategy: 'jwt',
-    },
-    callbacks: {
-      async jwt({ token, user }) {
-        // TODO: Implement refresh token
-        if (user) {
-          token = user;
-        }
-        return token;
-      },
-      async session({ session, token }) {
-        session.token = token.token as string;
-        session.refresh_token = token.refresh_token as string;
-        return session;
-      },
-    },
-  });
-}
+export default NextAuth(config);
--- apps/website/types/next-auth.d.ts
+++ apps/website/types/next-auth.d.ts
@@ -1,16 +1,20 @@
 /* REASON: Import needs to exists so typescript can merge definitions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* REASON: We can only override interfaces like this */
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import NextAuth from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 
-import { SessionUser } from './user';
+import { SessionUser, SessionToken } from './user';
 
 declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session {
-    user: SessionUser;
-    token: string;
-    refresh_token: string;
-  }
+  interface User extends SessionToken {}
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  type JWT = SessionToken;
 }
