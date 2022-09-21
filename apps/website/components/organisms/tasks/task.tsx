337f29758eb233d55a8cfd1163785b85b24ecb37
Manish Rawat <62084296+R11manish@users.noreply.github.com>
2022-09-21 07:43:35 +0530

    feat : add label bar for gate completation (#165)

--- apps/website/components/organisms/tasks/task.tsx
+++ apps/website/components/organisms/tasks/task.tsx
@@ -176,6 +176,7 @@ export function Task({
       sx={(theme) => ({
         borderRadius: 0,
         borderLeft: 'none',
+        borderTop : 'none',
         backgroundColor: 'transparent !important',
         backgroundImage: 'none !important',
         px: { xs: theme.spacing(1), md: theme.spacing(7) },
--- apps/website/components/templates/gate-view/gate-view.tsx
+++ apps/website/components/templates/gate-view/gate-view.tsx
@@ -161,6 +161,22 @@ export function GateViewTemplate({ gateProps }: GateViewProps) {
       }
     );
 
+  const { data: gateProgress } = useQuery(
+    ['gate_progress', gateProps?.id, me?.id],
+    () =>
+      gqlAuthMethods.GateProgress({
+        gateID: gateProps?.id,
+        userID: me?.id,
+      })
+  );
+
+  const completedAt = gateProgress?.gate_progress[0].completed_at;
+
+  const formattedDate = new Date(completedAt?.toLocaleString()).toLocaleString(
+    'en-us',
+    { hour12: true }
+  );
+
   const gateOptions = [
     {
       text:
@@ -450,27 +466,43 @@ export function GateViewTemplate({ gateProps }: GateViewProps) {
             margin: { xs: '16px 16px 40px 16px', md: '60px' },
           }}
         >
-          <CircularProgressWithLabel
-            variant="determinate"
-            value={(completedTasksCount / gateProps?.tasks.length) * 100}
-            label={`${completedTasksCount}/${gateProps?.tasks.length}`}
-            size={50}
-            thickness={4}
-            sx={{
-              color: '#6DFFB9',
-            }}
-          />
-          <Stack
-            sx={{
-              marginLeft: (theme) => theme.spacing(4),
-            }}
-          >
-            <Typography variant="h6">Tasks</Typography>
-            <Typography variant="caption">
-              Complete the tasks to unlock this credential
-            </Typography>
-          </Stack>
+          <Box display={'flex'}>
+            <CircularProgressWithLabel
+              variant="determinate"
+              value={(completedTasksCount / gateProps?.tasks.length) * 100}
+              label={`${completedTasksCount}/${gateProps?.tasks.length}`}
+              size={50}
+              thickness={4}
+              sx={{
+                color: '#6DFFB9',
+              }}
+            />
+            <Stack
+              sx={{
+                marginLeft: (theme) => theme.spacing(4),
+              }}
+            >
+              <Typography variant="h6">Tasks</Typography>
+              <Typography variant="caption">
+                Complete the tasks to unlock this credential
+              </Typography>
+            </Stack>
+          </Box>
         </Stack>
+        {completedAt && (
+          <Typography
+            sx={{
+              marginX: TOKENS.CONTAINER_PX,
+              py: 1,
+              px: 4,
+              border: 1,
+              borderRadius: 1,
+            }}
+            color={'#c5ffe3'}
+          >
+            You have completed this credential at {formattedDate}
+          </Typography>
+        )}
 
         <TaskGroup>
           {gateProps?.tasks.map((task, idx) => (
