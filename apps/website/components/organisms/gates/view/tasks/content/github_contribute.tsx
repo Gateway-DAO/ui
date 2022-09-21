a23721b895b6bc6afe8bd6e086a99c9ed315efaa
fbenfraj <farouk.ben-fraj@outlook.com>
2022-09-21 15:48:22 +0200

    move github contribute task completion to backend

--- apps/website/components/organisms/gates/view/tasks/content/github_contribute.tsx
+++ apps/website/components/organisms/gates/view/tasks/content/github_contribute.tsx
@@ -8,12 +8,14 @@ import GithubDataCard from '../../../../../../components/organisms/tasks/github-
 
 const GithubContributeContent = ({
   data,
+  completeTask,
   completed,
   updatedAt,
   readOnly,
   isLoading,
 }) => {
   const formattedDate = new Date(updatedAt.toLocaleString()).toLocaleString();
+  const githubAccessToken = window.localStorage.getItem('github_access_token');
   const { repository_link } = data;
 
   const [repository, setRepository] = useState({
@@ -25,14 +27,15 @@ const GithubContributeContent = ({
     forks_count: 0,
   });
 
+  const repository_owner = repository_link
+    .replace('https://github.com/', '')
+    .split('/')[0];
+  const repository_name = repository_link
+    .replace('https://github.com/', '')
+    .split('/')[1];
+
   useEffect(() => {
     const fetchRepository = async () => {
-      const repository_owner = repository_link
-        .replace('https://github.com/', '')
-        .split('/')[0];
-      const repository_name = repository_link
-        .replace('https://github.com/', '')
-        .split('/')[1];
       const response = await fetch(
         `https://api.github.com/repos/${repository_owner}/${repository_name}`
       );
@@ -49,26 +52,39 @@ const GithubContributeContent = ({
     };
 
     fetchRepository();
-  }, [repository_link]);
+  }, [repository_link, repository_owner, repository_name]);
 
   return (
     <Stack alignItems="start">
-      {!readOnly && !completed && (
-        <LoadingButton
-          variant="contained"
-          sx={{ marginTop: '15px' }}
-          isLoading={isLoading}
-        >
-          Submit
-        </LoadingButton>
-      )}
       <Typography variant="body2" padding={'30px 0'}>
         You must contribute to
       </Typography>
       <GithubDataCard data={repository} />
-      <GithubConnectionCard />
+      {!completed && !readOnly && githubAccessToken && (
+        <LoadingButton
+          isLoading={isLoading}
+          variant="contained"
+          sx={{
+            marginTop: '30px',
+          }}
+          onClick={() =>
+            completeTask({
+              info: { githubAccessToken, repository_name, repository_owner },
+            })
+          }
+        >
+          Verify
+        </LoadingButton>
+      )}
+      {!completed && !readOnly && !githubAccessToken && (
+        <GithubConnectionCard />
+      )}
       {completed && !!updatedAt && (
-        <Typography color="#c5ffe3" variant="subtitle2">
+        <Typography
+          color="#c5ffe3"
+          variant="subtitle2"
+          sx={{ paddingTop: '20px' }}
+        >
           Task completed at {formattedDate}
         </Typography>
       )}
