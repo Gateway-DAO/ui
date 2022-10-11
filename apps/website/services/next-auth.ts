5175ae755f1cedeac970d4e53c0339e866dbab19
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-10-11 15:02:36 -0300

    feat: add error on token response

--- apps/website/services/next-auth.ts
+++ apps/website/services/next-auth.ts
@@ -8,27 +8,23 @@ const callLogin = async (
   signature: string,
   wallet: string
 ): Promise<SessionToken> => {
-  try {
-    const res = await gqlAnonMethods.login({
-      signature,
-      wallet,
-    });
+  const res = await gqlAnonMethods.login({
+    signature,
+    wallet,
+  });
 
-    const { error } = (res as any) ?? {};
+  const { error } = (res as any) ?? {};
 
-    if (error || !res.login) {
-      throw error;
-    }
-
-    const { __typename, ...token } = res.login;
-
-    return {
-      ...token,
-      expiry: Date.parse(token.expiry),
-    };
-  } catch (e) {
-    return null;
+  if (error || !res.login) {
+    throw error;
   }
+
+  const { __typename, ...token } = res.login;
+
+  return {
+    ...token,
+    expiry: Date.parse(token.expiry),
+  };
 };
 const callRefresh = async (token: SessionToken): Promise<SessionToken> => {
   try {
@@ -50,7 +46,10 @@ const callRefresh = async (token: SessionToken): Promise<SessionToken> => {
       expiry: Date.parse(newToken.expiry),
     };
   } catch (e) {
-    return null;
+    return {
+      ...token,
+      error: e,
+    };
   }
 };
 
@@ -91,12 +90,13 @@ export const nextAuthConfig: NextAuthOptions = {
         const refreshedToken = await callRefresh(token);
         return refreshedToken;
       }
+
       return token;
     },
     async session({ session, token }) {
       return {
         ...session,
-        ...(token ?? {}),
+        ...token,
       };
     },
   },
