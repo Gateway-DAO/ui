f6384d247a109f0d5547101a24be4dbc37355d76
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-10-28 14:05:56 +0200

    feat: invalid wallets warning

--- apps/website/components/templates/create-gate/tasks/direct/direct-wallets-header.tsx
+++ apps/website/components/templates/create-gate/tasks/direct/direct-wallets-header.tsx
@@ -28,38 +28,45 @@ export function DirectWalletsHeader({ readFiles, walletsQueries }: Props) {
   );
 
   return (
-    <Stack
-      direction={{
-        xs: 'column',
-        lg: 'row',
-      }}
-      alignItems={{
-        xs: 'flex-start',
-        lg: 'center',
-      }}
-      justifyContent="space-between"
-      gap={2}
-    >
-      <Box>
-        <Typography variant="h6">{validWallets} recipients</Typography>
-        <Typography variant="body1" color="text.secondary">
-          Copy and paste, fill, import the wallet address and/or ens name
+    <Stack direction="column" gap={2}>
+      <Stack
+        direction={{
+          xs: 'column',
+          lg: 'row',
+        }}
+        alignItems={{
+          xs: 'flex-start',
+          lg: 'center',
+        }}
+        justifyContent="space-between"
+        gap={2}
+      >
+        <Box>
+          <Typography variant="h6">{validWallets} recipients</Typography>
+          <Typography variant="body1" color="text.secondary">
+            Copy and paste, fill, import the wallet address and/or ens name
+          </Typography>
+        </Box>
+        <Button component="label" variant="outlined" startIcon={<UploadFile />}>
+          Import from a CSV
+          <input
+            hidden
+            type="file"
+            accept=".csv"
+            onChange={(event) => {
+              if (event.target.files?.length) {
+                readFiles(event.target.files);
+              }
+            }}
+            value={[]}
+          />
+        </Button>
+      </Stack>
+      {invalidWallets ? (
+        <Typography variant="body2" color="error">
+          You have {invalidWallets} invalid wallets
         </Typography>
-      </Box>
-      <Button component="label" variant="outlined" startIcon={<UploadFile />}>
-        Import from a CSV
-        <input
-          hidden
-          type="file"
-          accept=".csv"
-          onChange={(event) => {
-            if (event.target.files?.length) {
-              readFiles(event.target.files);
-            }
-          }}
-          value={[]}
-        />
-      </Button>
+      ) : null}
     </Stack>
   );
 }
