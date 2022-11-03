d09ab72da522023c5d44805a54cfd1ebf3598a1e
Lucas Inacio <lucas1inacio7souza@gmail.com>
2022-11-03 10:33:20 -0300

    chore: refactor create gate mutation for task based type

--- apps/website/components/templates/create-gate/create-gate.tsx
+++ apps/website/components/templates/create-gate/create-gate.tsx
@@ -32,6 +32,9 @@ type CreateGateProps = {
   oldData?: CreateGateData;
 };
 
+// TODO: Delete unused tasks when changing gate type or when editing a gate
+// Delete all tasks and create the new ones.
+
 export function CreateGateTemplate({ oldData }: CreateGateProps) {
   const gateDetails = (({
     title,
@@ -52,62 +55,51 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
   const router = useRouter();
   const { gqlAuthMethods } = useAuth();
 
-  const [gateId, setGateId] = useState(oldData.id || '');
   const [confirmPublish, setConfirmPublish] = useState(false);
   const [isPublished, setIsPublished] = useState(false);
-  const [draftIsLoading, setDraftIsLoading] = useState(false);
-  const [createIsLoading, setCreateIsLoading] = useState(false);
   const [result, setResult] = useState(null);
 
   const [deletedTasks, setDeletedTasks] = useState<string[]>([]);
 
   const { enqueueSnackbar } = useSnackbar();
 
-  const { mutateAsync: uploadImage } = useMutation(
-    ['uploadImage'],
-    gqlAuthMethods.upload_image
-  );
+  const uploadImage = useMutation(['uploadImage'], gqlAuthMethods.upload_image);
 
-  const { mutateAsync: createGateMutation } = useMutation(
+  const createGate = useMutation(
     ['createGate'],
-    gqlAuthMethods.create_gate
+    gqlAuthMethods.create_gate_tasks_based
   );
 
-  const { mutate: publishGate } = useMutation(
-    ['publishGate'],
-    gqlAuthMethods.publish_gate
-  );
+  const publishGate = useMutation(['publishGate'], gqlAuthMethods.publish_gate);
 
-  const { mutateAsync: deleteTaskMutation } = useMutation(
+  const deleteTask = useMutation(
     ['deleteTask'],
     gqlAuthMethods.delete_tasks_by_pk
   );
 
   const closePublishedModal = () => setIsPublished(false);
 
-  const checkFormErrors = async (isDraft) => {
+  const checkFormErrors = async () => {
     const dataIsValid = await methods.trigger();
 
     if (!dataIsValid) {
       const errors = methods.formState.errors;
       enqueueSnackbar(
         Object.values(errors)[0].data?.message ||
-          taskErrorMessage(errors?.tasks?.data) ||
+          taskErrorMessage(errors?.tasks) ||
           'Invalid data',
         {
           variant: 'error',
         }
       );
-      isDraft ? setDraftIsLoading(false) : setCreateIsLoading(false);
     }
 
     return dataIsValid;
   };
 
   const handleMutation = async (data: CreateGateData, isDraft: boolean) => {
-    isDraft ? setDraftIsLoading(true) : setCreateIsLoading(true);
+    const dataIsValid = await checkFormErrors();
 
-    const dataIsValid = await checkFormErrors(isDraft);
     if (!dataIsValid) return;
 
     let permissionsData = null;
@@ -120,103 +112,62 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
       };
     }
     if (image_url !== data.image && data.image !== undefined) {
-      await uploadImage(
-        {
+      const image_id = (
+        await uploadImage.mutateAsync({
           base64: data.image,
           name: data.title,
-        },
-        {
-          onSuccess(imageData) {
-            const image_id = imageData['upload_image'].id;
-            image_url =
-              process.env.NEXT_PUBLIC_NODE_ENDPOINT +
-              '/storage/file?id=' +
-              image_id;
-          },
-          onError() {
-            enqueueSnackbar("An error occured, couldn't upload the image.", {
-              variant: 'error',
-            });
-          },
-        }
-      );
+        })
+      )?.upload_image.id;
+      image_url = `${process.env.NEXT_PUBLIC_NODE_ENDPOINT}/storage/file?id=${image_id}`;
     }
     if (deletedTasks.length > 0) {
-      deletedTasks.forEach(async (task_id) => {
-        await deleteTaskMutation({
-          task_id,
-        });
-      });
-    }
-    if (data.title) {
-      await createGateMutation(
-        {
-          id: oldData.id || uuidv4(),
-          dao_id: router.query.dao,
-          title: data.title,
-          categories: data.categories || [],
-          description: data.description,
-          skills: data.skills || [],
-          permissions: {
-            ...permissionsData,
-            on_conflict: {
-              constraint:
-                Permissions_Constraint.PermissionsDaoIdUserIdCredentialIdKey,
-              update_columns: [Permissions_Update_Column.Permission],
-            },
-          },
-          image: image_url,
-          tasks: {
-            data: data.tasks.data.map((task, index) => {
-              const { task_id, ...cleanTask } = task;
-              return {
-                ...cleanTask,
-                id: task_id,
-                order: index,
-              };
-            }),
-            on_conflict: {
-              constraint: Tasks_Constraint.KeysPk,
-              update_columns: [
-                Tasks_Update_Column.Title,
-                Tasks_Update_Column.Description,
-                Tasks_Update_Column.TaskData,
-                Tasks_Update_Column.TaskType,
-                Tasks_Update_Column.Order,
-              ],
-            },
-          },
-          published: 'not_published',
-        },
-        {
-          async onSuccess(result) {
-            if (isDraft) {
-              enqueueSnackbar('Draft saved');
-              setDraftIsLoading(false);
-              router.push(
-                ROUTES.GATE_PROFILE.replace('[id]', result.insert_gates_one.id)
-              );
-            } else {
-              await publishGate({
-                gate_id: result.insert_gates_one.id,
-              });
-              setGateId(result.insert_gates_one.id);
-              setResult(result.insert_gates_one);
-              setIsPublished(true);
-            }
-          },
-          onError() {
-            isDraft ? setDraftIsLoading(false) : setCreateIsLoading(false);
-            enqueueSnackbar(
-              isDraft
-                ? "An error occured, couldn't save the draft."
-                : "An error occured, couldn't create the gate."
-            );
-          },
-        }
+      await Promise.all(
+        deletedTasks.map((task_id) =>
+          deleteTask.mutateAsync({
+            task_id,
+          })
+        )
       );
     }
-    isDraft ? setDraftIsLoading(false) : setCreateIsLoading(false);
+    if (data.title) {
+      delete data.created_by;
+      const response = await createGate.mutateAsync({
+        ...data,
+        id: oldData.id || uuidv4(),
+        dao_id: router.query.dao,
+        title: data.title,
+        categories: data.categories || [],
+        description: data.description,
+        skills: data.skills || [],
+        permissions: {
+          ...permissionsData,
+          on_conflict: {
+            constraint:
+              Permissions_Constraint.PermissionsDaoIdUserIdCredentialIdKey,
+            update_columns: [Permissions_Update_Column.Permission],
+          },
+        },
+        image: image_url,
+        tasks: data.tasks.map(({ task_id, ...task }, index) => ({
+          ...task,
+          id: task_id,
+          order: index,
+        })),
+        published: 'not_published',
+      });
+      if (isDraft) {
+        enqueueSnackbar('Draft saved');
+        router.push(
+          ROUTES.GATE_PROFILE.replace('[id]', response.insert_gates_one.id)
+        );
+        return;
+      }
+      await publishGate.mutateAsync({
+        gate_id: response.insert_gates_one.id,
+      });
+      setResult(response.insert_gates_one);
+      setIsPublished(true);
+    }
   };
 
   const taskErrorMessage = (data): string | null => {
@@ -241,11 +192,21 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
 
   const gateType: GateType = methods.watch('type');
 
-  const saveDraft = (draftData: CreateGateData) =>
-    handleMutation(draftData, true);
+  const onSaveDraft = async (draftData: CreateGateData) => {
+    try {
+      await handleMutation(draftData, true);
+    } catch (e) {
+      enqueueSnackbar("An error occured, couldn't save the draft.");
+    }
+  };
 
-  const createGate = (gateData: CreateGateData) =>
-    handleMutation(gateData, false);
+  const onCreateGate = async (gateData: CreateGateData) => {
+    try {
+      await handleMutation(gateData, false);
+    } catch (e) {
+      enqueueSnackbar("An error occured, couldn't save the draft.");
+    }
+  };
 
   const hasTitleAndDescription = methods
     .watch(['title', 'description'])
@@ -263,7 +224,7 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
           onSubmit={async (e) => {
             e.preventDefault();
 
-            const dataIsValid = await checkFormErrors(false);
+            const dataIsValid = await checkFormErrors();
 
             if (dataIsValid) {
               setConfirmPublish(true);
@@ -276,9 +237,12 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
           })}
         >
           <PublishNavbar
-            draftIsLoading={draftIsLoading}
-            createIsLoading={createIsLoading}
-            saveDraft={saveDraft}
+            isLoading={
+              createGate.isLoading ||
+              publishGate.isLoading ||
+              deleteTask.isLoading
+            }
+            saveDraft={onSaveDraft}
           />
           <Typography
             component="h1"
@@ -404,12 +368,10 @@ export function CreateGateTemplate({ oldData }: CreateGateProps) {
             positiveAnswer="Publish"
             negativeAnswer="Cancel"
             setOpen={setConfirmPublish}
-            onConfirm={methods.handleSubmit(createGate, (errors) => {
+            onConfirm={methods.handleSubmit(onCreateGate, (errors) => {
               enqueueSnackbar(
                 Object.values(errors)[0].data?.message || 'Invalid data'
               );
-              setCreateIsLoading(false);
-              return;
             })}
           >
             If you publish this credential, you will no longer be allowed to
