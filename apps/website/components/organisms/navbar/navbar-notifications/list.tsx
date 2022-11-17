0732fa850e784c259594e00a07e488b9adc81248
fbenfraj <farouk.ben-fraj@frajtech.com>
2022-11-17 14:28:20 +0100

    add fetch notifications from redis

--- apps/website/components/organisms/navbar/navbar-notifications/list.tsx
+++ apps/website/components/organisms/navbar/navbar-notifications/list.tsx
@@ -1,4 +1,6 @@
 import { Stack } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { useAuth } from 'apps/website/providers/auth';
 
 import { useCyberConnect } from '../../../../providers/cyberconnect';
 import { NotificationType } from '../../../../services-cyberconnect/types.generated';
@@ -6,9 +8,40 @@ import { EmptyNotifications } from './empty';
 import { NotificationMethods } from './item-methods';
 import { AcceptedConnectionNotification } from './notifications/accepted-connection';
 import { NewConnectionNotification } from './notifications/new-connection';
+import { CustomNotification } from './notifications/custom';
 
 export function NotificationList() {
-  const { isLoading, notifications } = useCyberConnect();
+  const { isLoading, notifications: CCNotifications } = useCyberConnect();
+  const { me } = useAuth();
+
+  const { data: redisNotifications } = useQuery(
+    ['user-notifications', me?.id],
+    async () => {
+      const res = await fetch(`/api/notifications?userId=${me?.id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await res.json();
+      const notifications = JSON.parse(data.notifications);
+
+      return notifications;
+    },
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+    }
+  );
+
+  const notifications = [
+    ...CCNotifications,
+    ...(redisNotifications || []),
+  ].sort(function (x, y) {
+    return y.timestamp - x.timestamp;
+  });
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -20,28 +53,45 @@ export function NotificationList() {
 
   return (
     <Stack>
-      {notifications.map((notification, index) => (
-        <NotificationMethods key={notification.id} {...notification}>
-          {(methods) => (
-            <>
-              {notification.type === NotificationType.BiconnectReceived && (
-                <NewConnectionNotification
-                  {...notification}
-                  {...methods}
-                  isLast={notifications.length - 1 === index}
-                />
-              )}
-              {notification.type === NotificationType.BiconnectAccepted && (
-                <AcceptedConnectionNotification
-                  {...notification}
-                  {...methods}
-                  isLast={notifications.length - 1 === index}
-                />
-              )}
-            </>
-          )}
-        </NotificationMethods>
-      ))}
+      {notifications.map((notification, index) => {
+        const isCustom = notification.hasOwnProperty('event_type');
+        let notificationData = {};
+
+        if (isCustom) {
+          const { event_type, opened, createdAt, ...rest } = notification;
+          notificationData = rest;
+        }
+
+        return isCustom ? (
+          <CustomNotification
+            event_type={notification.event_type}
+            opened={notification.opened}
+            timestamp={notification.timestamp}
+            data={notificationData}
+          />
+        ) : (
+          <NotificationMethods key={notification.id} {...notification}>
+            {(methods) => (
+              <>
+                {notification.type === NotificationType.BiconnectReceived && (
+                  <NewConnectionNotification
+                    {...notification}
+                    {...methods}
+                    isLast={CCNotifications.length - 1 === index}
+                  />
+                )}
+                {notification.type === NotificationType.BiconnectAccepted && (
+                  <AcceptedConnectionNotification
+                    {...notification}
+                    {...methods}
+                    isLast={CCNotifications.length - 1 === index}
+                  />
+                )}
+              </>
+            )}
+          </NotificationMethods>
+        );
+      })}
     </Stack>
   );
 }
---
+++ apps/website/components/organisms/navbar/navbar-notifications/notifications/custom.tsx
@@ -0,0 +1,113 @@
+import useTranslation from 'next-translate/useTranslation';
+import Link from 'next/link';
+
+import { Box, Stack, Typography } from '@mui/material';
+
+import { ROUTES } from '../../../../../constants/routes';
+import { useTimeAgo } from '../../../../../utils/time';
+import { AvatarFile } from '../../../../atoms/avatar-file';
+import { useState } from 'react';
+
+type DataProps = {
+  dao_name?: string;
+  dao_img_url?: string;
+  gate_id?: string;
+  gate_name?: string;
+};
+
+type Props = {
+  event_type: string;
+  opened: boolean;
+  timestamp: string;
+  data: DataProps;
+  isLast?: boolean;
+  onRead?: () => void;
+};
+
+export function CustomNotification({
+  event_type,
+  opened,
+  timestamp,
+  isLast,
+  data,
+}: Props) {
+  const { t } = useTranslation('gate-new');
+  const timeAgo = useTimeAgo(timestamp);
+
+  const daoProfileUrl = ROUTES.DAO_PROFILE.replace(
+    '[slug]',
+    data.dao_name?.toLowerCase()
+  );
+  const gateUrl = ROUTES.GATE_PROFILE.replace('[id]', data.gate_id);
+
+  const [hasRead, setHasRead] = useState(opened);
+
+  const onRead = () => {
+    !hasRead && setHasRead(true);
+  };
+
+  return (
+    <Stack
+      component="div"
+      direction="column"
+      gap={1.75}
+      sx={[
+        { px: 2, pt: 0.5, pb: !isLast ? 1.75 : 4 },
+        !hasRead && { backgroundColor: '#9A53FF29' },
+      ]}
+      onMouseEnter={onRead}
+      onFocusCapture={onRead}
+    >
+      <Stack direction="row" gap={2} alignItems="center">
+        <Link passHref href={daoProfileUrl}>
+          <AvatarFile
+            file={data.dao_img_url as any}
+            component="a"
+            target="_blank"
+          />
+        </Link>
+        <Box>
+          <Typography variant="body1">
+            <Link passHref href={daoProfileUrl}>
+              <Typography
+                component="a"
+                color="text.primary"
+                target="_blank"
+                sx={{ textDecoration: 'none' }}
+              >
+                <Typography
+                  component="span"
+                  sx={{ textDecoration: 'underline' }}
+                >
+                  {data.dao_name}
+                </Typography>
+              </Typography>
+            </Link>{' '}
+            <Typography component="span" sx={{ opacity: 0.5 }}>
+              has published a new credential:
+            </Typography>
+            <Link passHref href={gateUrl}>
+              <Typography
+                component="a"
+                color="text.primary"
+                target="_blank"
+                sx={{ textDecoration: 'none' }}
+              >
+                <Typography
+                  component="span"
+                  sx={{ textDecoration: 'underline' }}
+                >
+                  {' '}
+                  {data.gate_name}
+                </Typography>
+              </Typography>
+            </Link>{' '}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {timeAgo}
+          </Typography>
+        </Box>
+      </Stack>
+    </Stack>
+  );
+}
