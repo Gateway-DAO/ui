9277ac295e3997a68eee1a7ce82c722d2c477ce4
Nuno Carvalho <nunoormaker21@gmail.com>
2022-12-03 17:51:12 +0000

    feat: created method for payed minting

--- apps/website/providers/biconomy/biconomy-provider.tsx
+++ apps/website/providers/biconomy/biconomy-provider.tsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ethers } from 'ethers';
 import { useSnackbar } from 'notistack';
 import { PartialDeep } from 'type-fest';
+import {
+  useAccount,
+  useContract,
+  useNetwork,
+  useSigner,
+  useSwitchNetwork,
+} from 'wagmi';
 
-import { Credentials } from '../../services/graphql/types.generated';
+import { CREDENTIAL_ABI } from '../../constants/web3';
+import {
+  Credentials,
+  Update_Credential_StatusMutation,
+} from '../../services/graphql/types.generated';
+import { getExplorer } from '../../utils/web3';
 import { useAuth } from '../auth';
 import { BiconomyContext, MintResponse } from './context';
 
 type ProviderProps = {
   children: React.ReactNode;
 };
-
-let provider;
-export let contract: ethers.Contract;
-export let contractInterface: ethers.ContractInterface;
 
 export type MintStatus = {
   [key: string]: {
@@ -25,9 +33,40 @@ export type MintStatus = {
   };
 };
 
+// const isGasless = process.env.NEXT_PUBLIC_GASLESS_MINTING === 'true';
+const isGasless = false;
+
 export function BiconomyProvider({ children }: ProviderProps) {
   // State
   const [mintStatus, setMintStatus] = useState<MintStatus>(() => ({}));
+
+  // From Wagmi
+  const {
+    data: signer,
+    refetch,
+    status,
+  } = useSigner({
+    onSettled(data, error) {
+      error && console.error('[BiconomyProvider]', error);
+      !data && status === 'success'
+        ? refetch()
+        : console.log('[BiconomyProvider]', data);
+    },
+  });
+  const { address } = useAccount();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+  const contract = useContract({
+    addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    signerOrProvider: signer,
+    contractInterface: CREDENTIAL_ABI,
+  });
+
+  useEffect(() => {
+    if (address && signer && contract) {
+      console.log('[BiconomyProvider]', 'Signer and Contract are ready');
+    }
+  }, [address, signer, contract]);
 
   // From auth
   const { me, gqlAuthMethods } = useAuth();
@@ -37,45 +76,67 @@ export function BiconomyProvider({ children }: ProviderProps) {
   // Credential update
   const queryClient = useQueryClient();
 
+  const mintOptions = {
+    onMutate: (id) => {
+      setMintStatus((prev) => ({
+        ...prev,
+        [id]: {
+          askingSignature: true,
+          isMinted: false,
+          error: null,
+        },
+      }));
+    },
+    onSuccess: (data, id) => {
+      setMintStatus((prev) => ({
+        ...prev,
+        [id]: {
+          askingSignature: false,
+          isMinted: true,
+          error: null,
+        },
+      }));
+
+      queryClient.invalidateQueries(['credentials']);
+      queryClient.resetQueries(['credential', id]);
+
+      queryClient.resetQueries(['user_info', me?.id]);
+    },
+    onError: (error) => {
+      enqueueSnackbar('Minting failed, please try again', {
+        variant: 'error',
+      });
+
+      console.log('[useMint] Error:', error);
+    },
+  };
+
   // Mint - backend
   const { mutateAsync: mintGasless } = useMutation(
     (id: string) => gqlAuthMethods.mint_credential({ id }),
-    {
-      onMutate: (id) => {
-        setMintStatus((prev) => ({
-          ...prev,
-          [id]: {
-            askingSignature: true,
-            isMinted: false,
-            error: null,
-          },
-        }));
-      },
-      onSuccess: (data, id) => {
-        setMintStatus((prev) => ({
-          ...prev,
-          [id]: {
-            askingSignature: false,
-            isMinted: true,
-            error: null,
-          },
-        }));
+    mintOptions
+  );
 
-        console.log(id);
+  const { mutateAsync: mint } = useMutation(
+    async (
+      credential: PartialDeep<Credentials>
+    ): Promise<Update_Credential_StatusMutation> => {
+      if (chain.id !== parseInt(process.env.NEXT_PUBLIC_CHAIN_ID)) {
+        await switchNetwork(parseInt(process.env.NEXT_PUBLIC_CHAIN_ID));
+      }
 
-        queryClient.invalidateQueries(['credentials']);
-        queryClient.resetQueries(['credential', id]);
+      const tx = await contract.mint(address, credential.uri);
 
-        queryClient.resetQueries(['user_info', me?.id]);
-      },
-      onError: (error) => {
-        enqueueSnackbar('Minting failed, please try again', {
-          variant: 'error',
-        });
-
-        console.log('[useMint] Error:', error);
-      },
-    }
+      // Update credential data
+      return await gqlAuthMethods.update_credential_status({
+        id: credential.id,
+        status: 'minted',
+        transaction_url: `${getExplorer(
+          parseInt(process.env.NEXT_PUBLIC_CHAIN_ID)
+        )}/tx/${tx.hash}`,
+      });
+    },
+    mintOptions
   );
 
   const mintCredential = async (
@@ -83,12 +144,18 @@ export function BiconomyProvider({ children }: ProviderProps) {
   ): Promise<MintResponse> => {
     try {
       // 2. mint the NFT
-      const { mint_credential: res } = await mintGasless(credential.id);
+      if (isGasless) {
+        const { mint_credential } = await mintGasless(credential.id);
 
-      return {
-        isMinted: true,
-        transactionUrl: res.info.transaction_hash,
-      };
+        return {
+          isMinted: true,
+          transactionUrl: mint_credential.info.transaction_hash,
+        };
+      }
+
+      const { update_credential_status: res } = await mint(credential);
+
+      return { isMinted: true, transactionUrl: res.transaction_url };
     } catch (error) {
       return {
         isMinted: false,
---
+++ apps/website/utils/web3.ts
@@ -0,0 +1,35 @@
+/**
+ * It returns a URL based on the chain ID
+ * @param {number} chainID - The chain ID of the network you want to connect to.
+ * @returns A string
+ */
+export const getExplorer = (chainID: number): string => {
+  switch (chainID) {
+    case 1:
+      return 'https://etherscan.io';
+    case 3:
+      return 'https://ropsten.etherscan.io';
+    case 4:
+      return 'https://rinkeby.etherscan.io';
+    case 5:
+      return 'https://goerli.etherscan.io';
+    case 6:
+      return 'https://kovan.etherscan.io';
+    case 10:
+      return 'https://optimistic.etherscan.io';
+    case 56:
+      return 'https://bscscan.com';
+    case 97:
+      return 'https://testnet.bscscan.com';
+    case 137:
+      return 'https://polygonscan.com';
+    case 42161:
+      return 'https://arbiscan.io';
+    case 80001:
+      return 'https://mumbai.polygonscan.com';
+    case 421611:
+      return 'https://rinkeby-explorer.arbitrum.io';
+    default:
+      return 'https://etherscan.io';
+  }
+};
