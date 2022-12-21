5273132647cd68071adee5f0d176ef7092e06b95
Manish Rawat <github.jmtss@gmail.com>
2022-12-21 19:31:02 +0530

    fix : typescript warning for recaptcha

--- apps/website/types/tasks.ts
+++ apps/website/types/tasks.ts
@@ -1,9 +1,9 @@
-import {
-  LinkPreviewOutput,
-  Scalars,
-} from '../services/hasura/types';
+import { LinkPreviewOutput, Scalars } from '../services/hasura/types';
 
-export type TaskType = Exclude<Scalars['task_type'], 'contract_interaction'>;
+export type TaskType = Exclude<
+  Scalars['task_type'] | 'recaptcha',
+  'contract_interaction'
+>;
 export type ManualTaskEventType = Scalars['manual_task_event_type'];
 
 export type ManualTaskEventData = {
