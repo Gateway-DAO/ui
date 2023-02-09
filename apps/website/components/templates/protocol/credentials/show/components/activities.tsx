4ec8cd6c2b020c135d85d13b4e531a2176f486bf
Nuno Carvalho <nunoormaker21@gmail.com>
2023-02-09 04:44:57 +0000

    feat: implemented activities

--- apps/website/components/templates/protocol/credentials/show/components/activities.tsx
+++ apps/website/components/templates/protocol/credentials/show/components/activities.tsx
@@ -24,11 +24,21 @@ import ExternalLink from '../../../../../atoms/external-link';
 import InternalLink from '../../../../../atoms/internal-link';
 
 type Props = {
-  activities: any[];
   credential: PartialDeep<Credential>;
 };
 
-export default function Activities({ activities, credential }: Props) {
+const activityText = (type: string) => {
+  return (
+    {
+      Issued: 'Credential issued',
+      Revoked: 'Credential revoked',
+      Suspended: 'Credential suspended',
+      Reactivated: 'Credential reactivated',
+    }[type] || 'Unknown activity'
+  );
+};
+
+export default function Activities({ credential }: Props) {
   const { t, lang } = useTranslation('protocol');
   const [expanded, setExpanded] = useState<boolean>(false);
   const router = useRouter();
@@ -102,21 +112,29 @@ export default function Activities({ activities, credential }: Props) {
         </AccordionSummary>
         <AccordionDetails sx={{ p: 0, m: 0 }}>
           <Stack sx={{ mb: 2 }} gap={1} divider={<Divider />}>
-            {activities?.map((activity, index) => (
-              <Stack key={index}>
-                <Typography fontSize={14}>{activity?.name}</Typography>
-                <Typography
-                  fontSize={12}
-                  sx={{ color: alpha(brandColors.white.main, 0.7) }}
-                >
-                  {timestampToString(
-                    activity?.date,
-                    lang,
-                    t('credential.indeterminate')
-                  )}
-                </Typography>
-              </Stack>
-            ))}
+            {credential.activities
+              ?.sort(
+                (a, b) =>
+                  new Date(b.timestamp).getTime() -
+                  new Date(a.timestamp).getTime()
+              )
+              .map((activity, index) => (
+                <Stack key={index}>
+                  <Typography fontSize={14}>
+                    {activityText(activity?.type)}
+                  </Typography>
+                  <Typography
+                    fontSize={12}
+                    sx={{ color: alpha(brandColors.white.main, 0.7) }}
+                  >
+                    {timestampToString(
+                      activity?.timestamp,
+                      lang,
+                      t('credential.indeterminate')
+                    )}
+                  </Typography>
+                </Stack>
+              ))}
           </Stack>
         </AccordionDetails>
       </Accordion>
