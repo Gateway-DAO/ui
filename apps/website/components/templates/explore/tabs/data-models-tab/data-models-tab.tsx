0592d8e055306a1de1bbf205631e69982d016a3b
André Felizardo <andreluis.felizardo@outlook.com>
2023-02-09 15:48:05 -0300

    WIP: data model tab

--- apps/website/components/templates/explore/tabs/data-models-tab/data-models-tab.tsx
+++ apps/website/components/templates/explore/tabs/data-models-tab/data-models-tab.tsx
@@ -1,8 +1,11 @@
-import Loading from 'apps/website/components/atoms/loading';
+import { useQuery } from 'wagmi';
 
 import { Box } from '@mui/material';
 
+import Loading from '../../../../../components/atoms/loading';
+
 export default function DataModelsTab(): JSX.Element {
+  const { data: dataModels } = useQuery([]);
   const isLoading = true;
   return <Box sx={{ py: 4 }}>{isLoading ? <Loading /> : <>carregado</>}</Box>;
 }
