09b87c1264d8a31048f79b9dd95d847461373eac
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-02-09 16:39:31 -0300

    fix: gateway id or wallet

--- apps/website/components/templates/protocol/credentials/create/components/recipient-form.tsx
+++ apps/website/components/templates/protocol/credentials/create/components/recipient-form.tsx
@@ -38,9 +38,9 @@ export default function RecipientForm() {
           }}
           label={t('data-model.issue-credential.recipient-label')}
           id="recipient-id"
-          {...register(`recipientUserIdOrWallet`)}
-          error={!!errors.recipientUserIdOrWallet}
-          helperText={errors.recipientUserIdOrWallet?.message}
+          {...register(`recipientUserGatewayIdOrWallet`)}
+          error={!!errors.recipientUserGatewayIdOrWallet}
+          helperText={errors.recipientUserGatewayIdOrWallet?.message}
         />
       </Stack>
     </Stack>
