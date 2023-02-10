1e023d19d9317b90e88e48a9a461323671f171ba
Siddharth Singh <63333707+Siddharth9890@users.noreply.github.com>
2023-02-10 19:54:27 +0530

    style: fix responsiveness of question delete component (#332)
    * style: fix responsiveness of question delete component
    
    * modified margin little bit more
    
    * style: added more margin to tasks

--- apps/website/components/molecules/add-task/github/contribute-task.tsx
+++ apps/website/components/molecules/add-task/github/contribute-task.tsx
@@ -123,9 +123,21 @@ export default function GithubContributeTask({
         <Stack
           direction={'row'}
           alignItems={'center'}
-          sx={{ width: '100%', mr: '20px' }}
+          sx={(theme) => ({
+            width: '100%',
+            mr: '20px',
+            [theme.breakpoints.between('md', 'lg')]: {
+              margin: '-22px',
+            },
+            [theme.breakpoints.between('lg', 'xl')]: {
+              margin: '-22px',
+            },
+          })}
         >
-          <TaskIcon type="github_contribute" sx={{ marginRight: 3 }} />
+          <TaskIcon
+            type="github_contribute"
+            sx={{ marginRight: 3, marginLeft: 4 }}
+          />
           <Stack>
             <Typography variant="subtitle2">
               {t('tasks.github_contribute.title')}
@@ -156,7 +168,18 @@ export default function GithubContributeTask({
           </Stack>
         </Stack>
         {!taskIsMoving && (
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box
+            sx={(theme) => ({
+              display: 'flex',
+              alignItems: 'center',
+              [theme.breakpoints.between('md', 'lg')]: {
+                marginLeft: '-55px',
+              },
+              [theme.breakpoints.between('lg', 'xl')]: {
+                marginLeft: '-55px',
+              },
+            })}
+          >
             <IconButton
               onClick={() => deleteTask(taskId)}
               sx={(theme) => ({
