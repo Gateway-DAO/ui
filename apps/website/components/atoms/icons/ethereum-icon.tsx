263d0dbefe1abf6d112fb76ac268df65532a1749
André Felizardo <andreluis.felizardo@outlook.com>
2023-02-28 11:45:14 -0300

    fix: update chain icons

--- apps/website/components/atoms/icons/ethereum-icon.tsx
+++ apps/website/components/atoms/icons/ethereum-icon.tsx
@@ -3,14 +3,14 @@ import { SvgIcon, SvgIconProps } from '@mui/material';
 export default function EthereumIcon(props: SvgIconProps) {
   return (
     <SvgIcon
-      width="11"
-      height="18"
-      viewBox="0 0 11 18"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M5.25003 0.666672L0 9.24128L5.25003 12.2998L10.5001 9.24128L5.25003 0.666672Z" />
-      <path d="M10.3319 10.1082L5.25001 13.2631L0.0482335 10.1082L5.25001 17.3333L10.3319 10.1082Z" />
+      <path d="M10.25 1.66669L5 10.2413L10.25 13.2998L15.5001 10.2413L10.25 1.66669Z" />
+      <path d="M15.3319 11.1082L10.25 14.2632L5.04823 11.1082L10.25 18.3334L15.3319 11.1082Z" />
     </SvgIcon>
   );
 }
