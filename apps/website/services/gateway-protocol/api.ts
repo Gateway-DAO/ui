e374a55e612d57f6a9628560a619f0ac1305ee65
Nuno Carvalho <nunoormaker21@gmail.com>
2023-03-09 18:12:36 +0000

    feat: add api key to protocol API (#399)

--- apps/website/services/gateway-protocol/api.ts
+++ apps/website/services/gateway-protocol/api.ts
@@ -5,13 +5,19 @@ import { getSdk } from './types';
 export type GatewayProtocolSDKTypes = ReturnType<typeof getSdk>;
 
 const gatewayProtocolClient = new GraphQLClient(
-  process.env.NEXT_PUBLIC_GATEWAY_PROTOCOL_ENDPOINT
+  process.env.NEXT_PUBLIC_GATEWAY_PROTOCOL_ENDPOINT,
+  {
+    headers: {
+      'x-api-key': process.env.NEXT_PUBLIC_PROTOCOL_API_KEY,
+    },
+  }
 );
 
 const gatewayProtocolAuthClient = (token: string) =>
   new GraphQLClient(process.env.NEXT_PUBLIC_GATEWAY_PROTOCOL_ENDPOINT, {
     headers: {
       Authorization: `Bearer ${token}`,
+      'x-api-key': process.env.NEXT_PUBLIC_PROTOCOL_API_KEY,
     },
   });
 
