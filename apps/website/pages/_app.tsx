9763233f9bcfdafd19e96ed5c88f795f0a966618
Rafa Gomes <rafagomes@users.noreply.github.com>
2023-03-09 15:25:16 -0300

    added google analytics  and Hotjar snippet (#394)
    * added google analytics snippet
    
    * Added Hotjar

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -66,7 +66,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
           </WalletProvider>
         </SessionProvider>
       </ThemeProvider>
-
+      {/* Smartlook */}
       <Script
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
@@ -78,6 +78,38 @@ function CustomApp({ Component, pageProps }: AppProps) {
             })(document);
             smartlook('init', '${process.env.NEXT_PUBLIC_SMARTLOOK_KEY}', { region: 'eu' });
             `,
+        }}
+      />
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-CKZ9WYMPM4"
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CKZ9WYMPM4');
+            `,
+        }}
+      />
+      {/* Hotjar */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3399024,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
     </>
