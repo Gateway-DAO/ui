4080d7c14a376cb7dbe8658b7db289611f2cbe30
João Bosco <boscoj@outlook.com>
2023-03-17 11:41:17 -0300

    Revert "Revert "feat: added restrictions to create a credential""

---
+++ apps/website/components/templates/protocol/data-models/show/components/issue-credential-button.tsx
@@ -0,0 +1,51 @@
+import useTranslation from 'next-translate/useTranslation';
+
+import { brandColors } from '@gateway/theme';
+
+import LockIcon from '@mui/icons-material/Lock';
+import { alpha, Button } from '@mui/material';
+
+type Props = {
+  onClickIssueCredential: () => void;
+  hasAnAccountAvailableToIssue: boolean;
+};
+
+export default function IssueCredentialButton({
+  onClickIssueCredential,
+  hasAnAccountAvailableToIssue,
+}: Props) {
+  const { t } = useTranslation('protocol');
+
+  return (
+    <>
+      {!hasAnAccountAvailableToIssue ? (
+        <Button
+          variant="contained"
+          disabled={true}
+          sx={{
+            width: '120px',
+            display: 'flex',
+            textTransform: 'uppercase',
+            gap: 1,
+            px: 1,
+          }}
+        >
+          <LockIcon
+            sx={{ fontSize: 16, color: alpha(brandColors.white.main, 0.5) }}
+          />{' '}
+          {t('data-model.actions.restricted')}
+        </Button>
+      ) : (
+        <Button
+          variant="contained"
+          sx={{ width: '180px' }}
+          onClick={() => {
+            onClickIssueCredential();
+          }}
+        >
+          {t('data-model.issue-credential-button')}
+        </Button>
+      )}
+    </>
+  );
+}
