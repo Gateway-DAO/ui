869c5be813e73f67c64c02f86882d55d7c7e7fdd
Manish Rawat <github.jmtss@gmail.com>
2023-03-21 18:24:11 +0530

    chore : added empty state message for wallets

--- apps/website/components/templates/gate-view/direct-holders-list/direct-holders-list.tsx
+++ apps/website/components/templates/gate-view/direct-holders-list/direct-holders-list.tsx
@@ -6,15 +6,19 @@ import { Virtuoso } from 'react-virtuoso';
 import { PartialDeep } from 'type-fest';
 
 import { TOKENS } from '@gateway/theme';
+import { GateFilled } from '@gateway/assets';
 
 import SearchIcon from '@mui/icons-material/Search';
 import {
+  Avatar,
   Box,
   Divider,
   Grid,
   InputAdornment,
   Stack,
+  SvgIcon,
   TextField,
+  Typography,
 } from '@mui/material';
 
 import { useAuth } from '../../../../providers/auth';
@@ -23,18 +27,22 @@ import { Gates } from '../../../../services/hasura/types';
 import { CenteredLoader } from '../../../atoms/centered-loader';
 import { UserListItem } from '../../../molecules/user-list-item';
 import { ClientNav } from '../../../organisms/navbar/client-nav';
+import { background } from '../../../../../../libs/theme/src/lib/config/colors';
 
 type Props = {
   gate: PartialDeep<Gates>;
   header: ReactNode;
   isLoading: boolean;
+  totalHolders: number;
 };
 
 export function DirectHoldersList({
   gate,
   header,
   isLoading: isLoadingInfo,
+  totalHolders,
 }: Props) {
+  const { t } = useTranslation('credential');
   const [filter, setFilter] = useState('');
   const { me } = useAuth();
 
@@ -135,7 +143,41 @@ export function DirectHoldersList({
         />
       </Box>
 
-      <Box sx={{ height: { lg: '100%', xs: '100vh' } }}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignSelf: 'center',
+          height: { lg: '100%', xs: '100vh' },
+        }}
+      >
+        {!totalHolders && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 339,
+              marginBottom: 20,
+            }}
+          >
+            <Box
+              sx={{
+                height: 64,
+                width: 64,
+                padding: 3,
+                borderRadius: '50%',
+                background: '#9a53ff36',
+              }}
+            >
+              <GateFilled />
+            </Box>
+
+            <Typography variant="h6" textAlign={'center'} height={64} mt={2}>
+              {t('direct-credential.eligibility.empty-whitelist')}
+            </Typography>
+          </Box>
+        )}
         {isLoading ? (
           <CenteredLoader />
         ) : (
