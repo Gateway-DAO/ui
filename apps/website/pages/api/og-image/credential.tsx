a92f04c9a68e4584d7ed5137603ab7f448b86831
André Felizardo <andreluis.felizardo@outlook.com>
2023-03-24 11:22:30 -0300

    fix: update visual structure

--- apps/website/pages/api/og-image/credential.tsx
+++ apps/website/pages/api/og-image/credential.tsx
@@ -53,187 +53,232 @@ const handler = (req: NextRequest) => {
           width: '100%',
           height: '100%',
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           color: '#fff',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '60%',
-            paddingLeft: '45px',
-            marginTop: '68px',
-          }}
-        >
-          <span
-            style={{ color: 'rgba(255, 255, 255, .7)', fontSize: '19.8px' }}
-          >
-            Credential ID {id}
-          </span>
-          <span style={{ fontWeight: '700', fontSize: '48px' }}>{title}</span>
-          <span
-            style={{ marginTop: '25px', fontWeight: '400', fontSize: '26px' }}
-          >
-            {description}
-          </span>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div
             style={{
               display: 'flex',
-              marginTop: '85px',
-              borderRadius: '28px',
-              alignItems: 'center',
-              background: 'rgba(255, 255, 255, 0.05)',
-              border: '2px solid rgba(229, 229, 229, 0.12)',
-              padding: '52px 26px',
-              justifyContent: 'space-between',
+              flexDirection: 'column',
+              width: '60%',
+              paddingLeft: '45px',
+              marginTop: '68px',
+            }}
+          >
+            <span
+              style={{ color: 'rgba(255, 255, 255, .7)', fontSize: '19.8px' }}
+            >
+              Credential ID {id}
+            </span>
+            <span style={{ fontWeight: '700', fontSize: '48px' }}>{title}</span>
+            <span
+              style={{ marginTop: '25px', fontWeight: '400', fontSize: '26px' }}
+            >
+              {description}
+            </span>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              width: '40%',
+              flexDirection: 'column',
+              position: 'relative',
+            }}
+          >
+            <img
+              src={`${origin}/images/share-top.png`}
+              alt="Symbols"
+              width="350px"
+              height="300px"
+            />
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '60%',
+              paddingLeft: '45px',
+              marginTop: '35px',
             }}
           >
             <div
               style={{
                 display: 'flex',
-                gap: '26px',
+                borderRadius: '28px',
                 alignItems: 'center',
+                background: 'rgba(255, 255, 255, 0.05)',
+                border: '2px solid rgba(229, 229, 229, 0.12)',
+                padding: '52px 26px',
+                justifyContent: 'space-between',
               }}
             >
-              <img
-                src={`${origin}/images/avatar-default.png`}
-                width="73px"
-                height="73px"
-                alt="Issuer profile image"
-              />
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <span
-                  style={{
-                    fontWeight: '400',
-                    fontSize: '19.8px',
-                    color: 'rgba(255, 255, 255, .7)',
-                  }}
-                >
-                  Issuer ID
-                </span>
-                <span
-                  style={{
-                    fontWeight: 400,
-                    fontSize: '23.1px',
-                    color: '#9A53FF',
-                  }}
-                >
-                  {issuer}
-                </span>
-              </div>
-            </div>
-            <img
-              src={`${origin}/images/arrow-transaction.png`}
-              width="28px"
-              height="56px"
-              alt="arrow transaction"
-              style={{
-                margin: '0 15px',
-              }}
-            />
-            <div
-              style={{
-                display: 'flex',
-                gap: '26px',
-                alignItems: 'center',
-              }}
-            >
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <span
-                  style={{
-                    fontWeight: '400',
-                    fontSize: '19.8px',
-                    color: 'rgba(255, 255, 255, .7)',
-                  }}
-                >
-                  Recipient ID
-                </span>
-                <span
-                  style={{
-                    fontWeight: 400,
-                    fontSize: '23.1px',
-                    color: '#9A53FF',
-                  }}
-                >
-                  {recipient}
-                </span>
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '26px',
+                  alignItems: 'center',
+                }}
+              >
+                <img
+                  src={`${origin}/images/avatar-default.png`}
+                  width="73px"
+                  height="73px"
+                  alt="Issuer profile image"
+                />
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span
+                    style={{
+                      fontWeight: '400',
+                      fontSize: '19.8px',
+                      color: 'rgba(255, 255, 255, .7)',
+                    }}
+                  >
+                    Issuer ID
+                  </span>
+                  <span
+                    style={{
+                      fontWeight: 400,
+                      fontSize: '23.1px',
+                      color: '#9A53FF',
+                    }}
+                  >
+                    {issuer}
+                  </span>
+                </div>
               </div>
               <img
-                src={`${origin}/images/avatar-default.png`}
-                width="73px"
-                height="73px"
-                alt="Recipient profile image"
+                src={`${origin}/images/arrow-transaction.png`}
+                width="28px"
+                height="56px"
+                alt="arrow transaction"
+                style={{
+                  margin: '0 15px',
+                }}
               />
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '26px',
+                  alignItems: 'center',
+                }}
+              >
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <span
+                    style={{
+                      fontWeight: '400',
+                      fontSize: '19.8px',
+                      color: 'rgba(255, 255, 255, .7)',
+                    }}
+                  >
+                    Recipient ID
+                  </span>
+                  <span
+                    style={{
+                      fontWeight: 400,
+                      fontSize: '23.1px',
+                      color: '#9A53FF',
+                    }}
+                  >
+                    {recipient}
+                  </span>
+                </div>
+                <img
+                  src={`${origin}/images/avatar-default.png`}
+                  width="73px"
+                  height="73px"
+                  alt="Recipient profile image"
+                />
+              </div>
             </div>
           </div>
-          <span
-            style={{
-              fontWeight: '400',
-              fontSize: '19.8px',
-              color: 'rgba(255, 255, 255, .7)',
-              marginTop: '25px',
-            }}
-          >
-            Issuance date {issuanceDate}
-          </span>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            width: '40%',
-            flexDirection: 'column',
-            position: 'relative',
-          }}
-        >
-          <img
-            src={`${origin}/images/share-top.png`}
-            alt="Symbols"
-            width="350px"
-            height="300px"
-          />
           <div
             style={{
               display: 'flex',
+              width: '40%',
+              flexDirection: 'column',
               marginTop: '35px',
-              width: '100%',
-              paddingRight: '25px',
-              gap: '15px',
-              justifyContent: 'flex-end',
             }}
           >
-            {hasImage && (
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                paddingRight: '25px',
+                gap: '15px',
+                justifyContent: 'flex-end',
+              }}
+            >
+              {hasImage && (
+                <img
+                  src={image}
+                  width="170px"
+                  height="170px"
+                  alt="Credential image"
+                  style={{
+                    borderRadius: '28px',
+                  }}
+                />
+              )}
               <img
-                src={image}
+                src={qrCode}
                 width="170px"
                 height="170px"
-                alt="Credential image"
+                alt="QR Code"
                 style={{
                   borderRadius: '28px',
                 }}
               />
-            )}
-            <img
-              src={qrCode}
-              width="170px"
-              height="170px"
-              alt="QR Code"
+            </div>
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '60%',
+              paddingLeft: '45px',
+              marginTop: '75px',
+              position: 'relative',
+            }}
+          >
+            <span
               style={{
-                borderRadius: '28px',
+                fontWeight: '400',
+                fontSize: '19.8px',
+                color: 'rgba(255, 255, 255, .7)',
+                position: 'relative',
+                top: '-55px',
+              }}
+            >
+              Issuance date {issuanceDate}
+            </span>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              width: '40%',
+              flexDirection: 'column',
+              position: 'relative',
+              marginTop: '75px',
+            }}
+          >
+            <img
+              src={`${origin}/images/share-bottom.png`}
+              alt="Symbol"
+              width="90px"
+              height="80px"
+              style={{
+                position: 'absolute',
+                bottom: '0',
+                right: '0',
               }}
             />
           </div>
-          <img
-            src={`${origin}/images/share-bottom.png`}
-            alt="Symbol"
-            width="90px"
-            height="80px"
-            style={{
-              position: 'absolute',
-              bottom: '0',
-              right: '0',
-            }}
-          />
         </div>
       </div>
     ),
