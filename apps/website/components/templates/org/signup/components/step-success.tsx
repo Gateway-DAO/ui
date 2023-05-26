9c43fa50e49aee63ad900d1f4931542582805124
André Felizardo <me@andrefelizardo.com.br>
2023-05-25 22:01:20 -0300

    feat: creating the org on database

--- apps/website/components/templates/org/signup/components/step-success.tsx
+++ apps/website/components/templates/org/signup/components/step-success.tsx
@@ -1,13 +1,21 @@
 import useTranslation from 'next-translate/useTranslation';
 
+import { useLocalStorage } from 'react-use';
+
 import { Typography, Stack, Box, Button } from '@mui/material';
 
+import { localStorageKeys } from '../../../../../constants/local-storage-keys';
+import { ROUTES } from '../../../../../constants/routes';
 import SuccessfullyIcon from '../../../../atoms/icons/successfully-icon';
 import TextWithParagraphs from '../../../../atoms/text-with-paragraphs/text-with-paragraphs';
 import Stepper from '../../../../organisms/stepper/stepper';
 
 export default function StepSuccess() {
   const { t } = useTranslation('org-signup');
+  const [formValue, updateFormValue] = useLocalStorage(
+    localStorageKeys.org_signup,
+    null
+  );
 
   return (
     <Stack>
@@ -20,7 +28,11 @@ export default function StepSuccess() {
       </Box>
       <TextWithParagraphs text={t('step-success.text')} />
       <Box mt={8}>
-        <Button variant="contained" size="large">
+        <Button
+          variant="contained"
+          size="large"
+          href={ROUTES.DAO_PROFILE.replace('[slug]', formValue?.slug)}
+        >
           {t('step-success.action')}
         </Button>
       </Box>
--- apps/website/components/templates/org/signup/signup.tsx
+++ apps/website/components/templates/org/signup/signup.tsx
@@ -1,8 +1,10 @@
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { useWindowSize } from 'react-use';
+import { useSnackbar } from 'notistack';
+import { useLocalStorage, useWindowSize } from 'react-use';
+import { useMutation } from 'wagmi';
 
 import { brandColors } from '@gateway/theme';
 
@@ -20,8 +22,12 @@ import {
   useTheme,
 } from '@mui/material';
 
+import { localStorageKeys } from '../../../../constants/local-storage-keys';
+import { mutation } from '../../../../constants/queries';
 import { ROUTES } from '../../../../constants/routes';
 import { useMultistepForm } from '../../../../hooks/use-multistep-form';
+import { useAuth } from '../../../../providers/auth';
+import Loading from '../../../atoms/loading';
 import FormStepper from '../../../molecules/form-stepper/form-stepper';
 import RealTimeView, { StepNames } from './components/real-time-view';
 import { setUpFormComponents } from './components/set-up-form-components';
@@ -37,10 +43,13 @@ export function OrgSignUpTemplate({
   const { t } = useTranslation('org-signup');
   const router = useRouter();
   const [fullFormState, setFullFormState] = useState(null);
+  const { gqlAuthMethods } = useAuth();
 
   const handleStep = (newValue: boolean) => {
     setStepValidity((prev) => ({ ...prev, [currentStep]: newValue }));
   };
+
+  const snackbar = useSnackbar();
 
   const formStepControl: { name: StepNames; backgroundImage: boolean }[] = [
     { name: '', backgroundImage: true },
@@ -55,6 +64,11 @@ export function OrgSignUpTemplate({
     { name: 'telegram', backgroundImage: true },
     { name: 'success', backgroundImage: true },
   ];
+
+  const [formValueStorage, updateFormValueStorage] = useLocalStorage(
+    localStorageKeys.org_signup,
+    null
+  );
 
   const formComponents = setUpFormComponents({
     fullFormState,
@@ -77,166 +91,200 @@ export function OrgSignUpTemplate({
 
   const handleNext = () => {
     changeStep(currentStep + 1);
-    router.push({ hash: formStepControl[currentStep + 1].name });
+    router.push({
+      hash: `org-signup_${formStepControl[currentStep + 1].name}`,
+    });
   };
 
   const handlePrevious = () => {
     changeStep(currentStep - 1);
-    router.push({ hash: formStepControl[currentStep - 1].name });
+    router.push({
+      hash: `org-signup${currentStep - 1 === 0 ? '' : '_'}${
+        formStepControl[currentStep - 1].name
+      }`,
+    });
   };
 
-  const handleSubmit = () => {
-    handleNext();
+  const handleSubmit = async () => {
+    await createOrganization.mutate();
   };
+
+  const createOrganization = useMutation(
+    [mutation.create_organization],
+    () =>
+      gqlAuthMethods.create_organization({
+        ...fullFormState,
+      }),
+    {
+      onSuccess: (data) => {
+        handleNext();
+        updateFormValueStorage({
+          slug: data.insert_daos_one.slug,
+        });
+      },
+      onError: (e: any) => {
+        snackbar.enqueueSnackbar(e?.response?.errors?.[0]?.message, {
+          variant: 'error',
+        });
+      },
+    }
+  );
 
   return (
-    <Grid
-      container
-      height={isMobile ? 'auto' : `100%`}
-      sx={{
-        flexWrap: 'nowrap',
-        flexDirection: { xs: 'column', md: 'row' },
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-        minHeight: `${windowSize.height}px`,
-        backgroundColor: brandColors.background.dark,
-        backgroundImage: formStepControl[currentStep].backgroundImage
-          ? 'url(/images/signup-background.png)'
-          : 'none',
-      }}
-    >
+    <>
+      {createOrganization.isLoading && <Loading fullScreen />}
       <Grid
-        item
-        xs={12}
-        md={6}
-        lg={4}
+        container
+        height={isMobile ? 'auto' : `100%`}
         sx={{
-          pt: { xs: 3, md: 6 },
-          pb: { xs: 3, md: 0 },
-          px: { xs: 2, md: 6 },
-          flexGrow: 0,
-          background: alpha(theme.palette.common.black, 0.03),
-          backdropFilter: 'blur(25px)',
-          height: '100%',
+          flexWrap: 'nowrap',
+          flexDirection: { xs: 'column', md: 'row' },
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover',
+          minHeight: `${windowSize.height}px`,
+          backgroundColor: brandColors.background.dark,
+          backgroundImage: formStepControl[currentStep].backgroundImage
+            ? 'url(/images/signup-background.png)'
+            : 'none',
         }}
       >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          sx={{ mb: { xs: isLastStep ? 0 : 4, md: isLastStep ? 0 : 9 } }}
-        >
-          {!isLastStep && (
-            <Box component="a" href={ROUTES.EXPLORE}>
-              <img
-                src="/favicon-192.png"
-                alt={t('logo-alternative-text')}
-                width="30"
-              />
-            </Box>
-          )}
-
-          <Avatar sx={{ display: { md: 'none' } }}>
-            <IconButton
-              onClick={() => {
-                closeDialog();
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-          </Avatar>
-        </Stack>
-        <Stack gap={5}>
-          {!isLastStep && (
-            <FormStepper
-              currentStep={currentStep}
-              qtdSteps={formComponents.length}
-            />
-          )}
-
-          <Stack>
-            {currentStepComponent}
-            <Stack direction="row" justifyContent="space-between" gap={2}>
-              {isFirstStep && (
-                <Button variant="contained" fullWidth onClick={handleNext}>
-                  {t('step-create-profile.action')}
-                </Button>
-              )}
-              {!isFirstStep && !isLastStep && (
-                <Button
-                  variant="outlined"
-                  size="large"
-                  onClick={handlePrevious}
-                >
-                  Back
-                </Button>
-              )}
-              {!isLastStep &&
-                !isFirstStep &&
-                formStepControl[currentStep].name !== 'telegram' && (
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    size="large"
-                    disabled={!stepValidity[`${currentStep}`]}
-                  >
-                    Next
-                  </Button>
-                )}
-              {formStepControl[currentStep].name === 'telegram' && (
-                <Button variant="contained" size="large" onClick={handleSubmit}>
-                  {fullFormState?.telegram?.length > 0
-                    ? 'Finish'
-                    : 'Skip and finish'}
-                </Button>
-              )}
-            </Stack>
-          </Stack>
-        </Stack>
-      </Grid>
-      <Divider orientation={isMobile ? 'horizontal' : 'vertical'} flexItem />
-      <Grid
-        item
-        xs={12}
-        md={6}
-        lg={8}
-        sx={{
-          pt: { xs: 3, md: 6 },
-          flexGrow: 1,
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Stack
-          justifyContent="end"
-          direction="row"
-          flexGrow={0}
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={4}
           sx={{
-            mb: 6,
+            pt: { xs: 3, md: 6 },
+            pb: { xs: 3, md: 0 },
             px: { xs: 2, md: 6 },
-            display: { xs: 'none', md: 'flex' },
+            flexGrow: 0,
+            background: alpha(theme.palette.common.black, 0.03),
+            backdropFilter: 'blur(25px)',
+            height: '100%',
           }}
         >
-          <Avatar>
-            <IconButton
-              onClick={() => {
-                closeDialog();
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-          </Avatar>
-        </Stack>
-        {!formStepControl[currentStep].backgroundImage && (
-          <RealTimeView
-            step={formStepControl[currentStep].name}
-            name={fullFormState?.name}
-            gatewayId={fullFormState?.gatewayId}
-            categories={fullFormState?.categories}
-            about={fullFormState?.about}
-          />
-        )}
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ mb: { xs: isLastStep ? 0 : 4, md: isLastStep ? 0 : 9 } }}
+          >
+            {!isLastStep && (
+              <Box component="a" href={ROUTES.EXPLORE}>
+                <img
+                  src="/favicon-192.png"
+                  alt={t('logo-alternative-text')}
+                  width="30"
+                />
+              </Box>
+            )}
+
+            <Avatar sx={{ display: { md: 'none' } }}>
+              <IconButton
+                onClick={() => {
+                  closeDialog();
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </Avatar>
+          </Stack>
+          <Stack gap={5}>
+            {!isLastStep && (
+              <FormStepper
+                currentStep={currentStep}
+                qtdSteps={formComponents.length}
+              />
+            )}
+
+            <Stack>
+              {currentStepComponent}
+              <Stack direction="row" justifyContent="space-between" gap={2}>
+                {isFirstStep && (
+                  <Button variant="contained" fullWidth onClick={handleNext}>
+                    {t('step-create-profile.action')}
+                  </Button>
+                )}
+                {!isFirstStep && !isLastStep && (
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    onClick={handlePrevious}
+                  >
+                    Back
+                  </Button>
+                )}
+                {!isLastStep &&
+                  !isFirstStep &&
+                  formStepControl[currentStep].name !== 'telegram' && (
+                    <Button
+                      variant="contained"
+                      onClick={handleNext}
+                      size="large"
+                      disabled={!stepValidity[`${currentStep}`]}
+                    >
+                      Next
+                    </Button>
+                  )}
+                {formStepControl[currentStep].name === 'telegram' && (
+                  <Button
+                    variant="contained"
+                    size="large"
+                    onClick={handleSubmit}
+                  >
+                    {fullFormState?.telegram?.length > 0
+                      ? 'Finish'
+                      : 'Skip and finish'}
+                  </Button>
+                )}
+              </Stack>
+            </Stack>
+          </Stack>
+        </Grid>
+        <Divider orientation={isMobile ? 'horizontal' : 'vertical'} flexItem />
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={8}
+          sx={{
+            pt: { xs: 3, md: 6 },
+            flexGrow: 1,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Stack
+            justifyContent="end"
+            direction="row"
+            flexGrow={0}
+            sx={{
+              mb: 6,
+              px: { xs: 2, md: 6 },
+              display: { xs: 'none', md: 'flex' },
+            }}
+          >
+            <Avatar>
+              <IconButton
+                onClick={() => {
+                  closeDialog();
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </Avatar>
+          </Stack>
+          {!formStepControl[currentStep].backgroundImage && (
+            <RealTimeView
+              step={formStepControl[currentStep].name}
+              name={fullFormState?.name}
+              gatewayId={fullFormState?.gatewayId}
+              categories={fullFormState?.categories}
+              about={fullFormState?.about}
+            />
+          )}
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
