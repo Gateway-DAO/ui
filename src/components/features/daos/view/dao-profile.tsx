57adedfe9776f80e89f5373c256103c2302a514e
Manish Rawat <github.jmtss@gmail.com>
2023-06-05 16:12:21 +0530

    fix : displaying earn credential issue

--- src/components/features/daos/view/dao-profile.tsx
+++ src/components/features/daos/view/dao-profile.tsx
@@ -72,7 +72,7 @@ export function DaoProfile() {
           <OverviewTab
             people={people}
             setTab={setTab}
-            credentials={credentials?.daos_by_pk.gates}
+            credentials={credentials?.daos_by_pk?.gates}
             loyaltyPrograms={loyaltyPrograms}
           />
         ),
@@ -116,7 +116,11 @@ export function DaoProfile() {
       });
     }
     return hasProtocolOrganization ? dApptabs.concat(protocolTabs) : dApptabs;
-  }, [hasProtocolOrganization, loyaltyPrograms]);
+  }, [
+    hasProtocolOrganization,
+    loyaltyPrograms,
+    credentials?.daos_by_pk?.gates,
+  ]);
 
   return (
     <>
