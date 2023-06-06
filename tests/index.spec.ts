d42ffb2fafb7af1914eba4b9f73c3f41b4267e49
valtrix <ifindu@localhost>
2023-06-06 16:37:04 -0300

    updated index test for better usage

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,23 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
 
-test('should navigate to the explore page', async ({ page }) => {
-  await page.goto('http://localhost:4200/');
-  await page.click('text=Open App');
-  await expect(page).toHaveURL('http://localhost:4200/explore');
+import { navigateTo } from './events/base.events';
+dotenv.config();
+
+test.describe('Index flow', () => {
+  let page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('should navigate to the explore page', async () => {
+    await navigateTo(page, '/');
+    await page.click('text=Open App');
+    await expect(page).toHaveURL(`${process.env.TEST_BASEURL}/explore`);
+  });
 });
