3bce7c42dfe182fd0c4b1d48bc3d39b82fca6a0d
Siddharth9890 <siddharth.a9890@gmail.com>
2023-06-28 14:47:39 +0530

    chore: formatted footer file using prettier vscode not doint it automatically

--- src/components/features/daos/view/tabs/overview-tab/quest/set-up-form-components.tsx
+++ src/components/features/daos/view/tabs/overview-tab/quest/set-up-form-components.tsx
@@ -2,8 +2,6 @@ import { Dispatch } from 'react';
 import { CreateQuestTemplate } from './create-quest';
 import CredentialTemplate from './components/credential-template';
 
-
-
 type Props = {
   updateFormState: Dispatch<any>;
   handleStep: (value: boolean) => void;
@@ -16,9 +14,23 @@ export const setUpFormComponents = ({
   fullFormState,
 }: Props): JSX.Element[] => {
   return [
-    <CredentialTemplate updateFormState={updateFormState} key={1} handleStep={handleStep} input={{ name: 'template', preview: false, saveAsDraft: false }} />,
-    <CredentialTemplate updateFormState={updateFormState} key={2} handleStep={handleStep} input={{ name: 'details', preview: false, saveAsDraft: false }} />,
-    <CredentialTemplate updateFormState={updateFormState} key={3} handleStep={handleStep} input={{ name: 'task', preview: true, saveAsDraft: false }} />,
-    
+    <CredentialTemplate
+      updateFormState={updateFormState}
+      key={1}
+      handleStep={handleStep}
+      input={{ name: 'template', preview: false, saveAsDraft: false }}
+    />,
+    <CredentialTemplate
+      updateFormState={updateFormState}
+      key={2}
+      handleStep={handleStep}
+      input={{ name: 'details', preview: false, saveAsDraft: false }}
+    />,
+    <CredentialTemplate
+      updateFormState={updateFormState}
+      key={3}
+      handleStep={handleStep}
+      input={{ name: 'task', preview: true, saveAsDraft: false }}
+    />,
   ];
 };
