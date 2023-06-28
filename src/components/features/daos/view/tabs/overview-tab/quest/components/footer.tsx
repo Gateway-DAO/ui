851b630a26ac472308bfd2135d67222c23ed4661
Siddharth9890 <siddharth.a9890@gmail.com>
2023-06-28 12:39:08 +0530

    chore: formatted footer file using prettier vscode not doint it automatically

--- src/components/features/daos/view/tabs/overview-tab/quest/components/footer.tsx
+++ src/components/features/daos/view/tabs/overview-tab/quest/components/footer.tsx
@@ -1,12 +1,24 @@
 import { Button, Divider, Stack } from '@mui/material';
 
-export default function Footer({fullFormState,handleNext}:{fullFormState:any,handleNext:any}) {
-  console.log(fullFormState)
+export default function Footer({
+  fullFormState,
+  handleNext,
+}: {
+  fullFormState: any;
+  handleNext: any;
+}) {
+  console.log(fullFormState);
   return (
     <>
       <Divider />
-      <Stack direction={"row"} sx={{mt:2}} justifyContent={"end"}>
-        <Button variant="contained" disabled={!fullFormState?.template?.preview} onClick={()=>handleNext()}>Preview</Button>
+      <Stack direction={'row'} sx={{ mt: 2 }} justifyContent={'end'}>
+        <Button
+          variant="contained"
+          disabled={!fullFormState?.template?.preview}
+          onClick={() => handleNext()}
+        >
+          Preview
+        </Button>
         <Button variant="contained">Save as Draft</Button>
         <Button variant="contained">Contained</Button>
       </Stack>
