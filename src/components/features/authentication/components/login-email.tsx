434512da3e705d70b1f28c8bc987206b78ff512d
Lucas Inacio <lucas1inacio7souza@gmail.com>
2023-07-04 14:36:23 -0300

    chore: removed useless key check

--- src/components/features/authentication/components/login-email.tsx
+++ src/components/features/authentication/components/login-email.tsx
@@ -23,10 +23,7 @@ export function LoginEmail() {
   });
   const { onSumbitEmail } = useSignUpContext();
 
-  const createEmailNonce = useMutation(
-    ['createEmailNonce'],
-    hasuraPublicService.create_email_nonce
-  );
+  const createEmailNonce = useMutation(hasuraPublicService.create_email_nonce);
 
   const onSubmit = async (data: EmailSchema) => {
     try {
