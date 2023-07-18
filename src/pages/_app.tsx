d7b7cfc7f9ad78a70137207ae4db98ea346b2a01
André Felizardo <me@andrefelizardo.com.br>
2023-07-18 11:05:25 -0300

    WIP: test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -76,18 +76,17 @@ function CustomApp({ Component, pageProps }: AppProps) {
       {process.env.NEXT_PUBLIC_GTM_TAG && (
         <>
           <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_TAG}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-MP7XQ5T8QJ`}
           />
           <Script
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_TAG}');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-MP7XQ5T8QJ');
               `,
             }}
           />
