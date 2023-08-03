c5c0187b5fb603cbcc12aeccda9ff756684d4dc8
Siddharth9890 <siddharth.a9890@gmail.com>
2023-08-03 10:18:21 +0530

    chore: added default image for all data models

--- src/components/molecules/cards/data-model-card.tsx
+++ src/components/molecules/cards/data-model-card.tsx
@@ -20,9 +20,10 @@ export function DataModelCard({
   tags,
   createdBy,
   permissioning,
+  image,
 }: PartialDeep<Protocol_Data_Model>): JSX.Element {
   const url = ROUTES.PROTOCOL_DATAMODEL.replace('[id]', id);
-  return permissioning === Protocol_Api_PermissionType.All ? (
+  return (
     <Link passHref href={url}>
       <MUICard
         sx={{
@@ -44,11 +45,6 @@ export function DataModelCard({
             pb: 1,
             '.MuiCardHeader-avatar': { width: '100%', mt: -2, mb: -1 },
             px: 0,
-            backgroundColor: '#9A53FF',
-            img: {
-              filter: 'grayscale(1)',
-              mixBlendMode: 'hard-light',
-            },
             ':hover': {
               img: {
                 filter: 'none',
@@ -59,9 +55,7 @@ export function DataModelCard({
           avatar={
             <img
               width={'100%'}
-              src={
-                'https://user-images.githubusercontent.com/63333707/234028818-2faa0548-20ed-483d-93b6-6e09d1308da9.png'
-              }
+              src={image ? image : '/images/default-image-data-models.png'}
               alt={title}
               height={'auto'}
             />
@@ -91,64 +85,6 @@ export function DataModelCard({
             {description}
           </Typography>
           <CategoriesList categories={tags as unknown as string[]} />
-        </CardContent>
-      </MUICard>
-    </Link>
-  ) : (
-    <Link passHref href={url}>
-      <MUICard
-        sx={{
-          position: 'relative',
-          cursor: 'pointer',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-        }}
-      >
-        <CardHeader
-          title={createdBy.gatewayUser?.name || createdBy?.gatewayId}
-          titleTypographyProps={{ fontSize: '14px', fontWeight: 400 }}
-          avatar={
-            <AvatarFile
-              file={createdBy.gatewayUser?.picture}
-              fallback={'/images/avatar.png'}
-              sx={{ width: 32, height: 32 }}
-            />
-          }
-        />
-        <CardContent
-          sx={{
-            py: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            flexGrow: 1,
-          }}
-        >
-          <Box flexGrow={1}>
-            <Typography gutterBottom variant="h5" sx={{ cursor: 'pointer' }}>
-              {title}
-            </Typography>
-            <Typography
-              height={40}
-              variant="body2"
-              color="text.secondary"
-              sx={{
-                display: '-webkit-box',
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: 'vertical',
-                overflow: 'hidden',
-              }}
-            >
-              {description}
-            </Typography>
-          </Box>
-
-          <Box mt={2}>
-            {tags && tags.length > 0 && (
-              <CategoriesList categories={tags as unknown as string[]} />
-            )}
-          </Box>
         </CardContent>
       </MUICard>
     </Link>
