ded1a7e1dede346cee9093c9e613e0bbfa96642f
Nuno Carvalho <nunoormaker21@gmail.com>
2023-08-08 13:37:41 +0100

    feat: implemented correct Altitude transactions on page

--- src/pages/api/altitude/index.ts
+++ src/pages/api/altitude/index.ts
@@ -9,13 +9,15 @@ type Cred = {
 export default async function handler(req, res) {
   const { wallet } = req.query;
 
-  try {
-    const creds = altitude as Cred[];
-    const tx = creds.find((cred) => cred.USER_ADDRESS === wallet.toLowerCase());
+  const creds = altitude as Cred[];
+  const tx = creds.find((cred) => cred.USER_ADDRESS === wallet.toLowerCase());
 
-    res.status(200).json(tx);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error });
+  if (tx) {
+    res.status(200).json({ tx });
+  } else {
+    res.status(200).json({
+      USER_ADDRESS: wallet,
+      USER_TOTAL_TRANSACTIONS: 0,
+    });
   }
 }
