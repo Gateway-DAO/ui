39dbe16ade16eecb8512d93c4ce06579f54a1021
Siddharth9890 <siddharth.a9890@gmail.com>
2023-08-24 09:41:14 +0530

    test:page not found using static prop

--- src/pages/explore/issue.tsx
+++ src/pages/explore/issue.tsx
@@ -3,6 +3,11 @@ import useTranslation from 'next-translate/useTranslation';
 import { ExploreLayout, DataModelsTab } from '@/components/features/explore';
 import { HeadContainer } from '@/components/molecules/head-container';
 
+export function getStaticProps() {
+  return {
+    notFound: true,
+  };
+}
 export default function Earn() {
   const { t } = useTranslation('explore');
 
