180b7f681c2d2662c377ab08ebfe6934518b2547
Manish Rawat <62084296+R11manish@users.noreply.github.com>
2023-10-06 17:29:23 +0530

    chore : sanket feedback (#772)

--- src/components/features/settings/navbar-settings/navbar-settings.tsx
+++ src/components/features/settings/navbar-settings/navbar-settings.tsx
@@ -63,20 +63,6 @@ export function NavBarSettings() {
       icon: <ManageAccounts {...iconAttr} />,
       href: ROUTES.SETTINGS_ACCOUNT_MANAGEMENT,
     },
-    {
-      key: 'notifications',
-      title: t('nav.notifications-title'),
-      description: t('nav.notifications-description'),
-      icon: <Notifications {...iconAttr} />,
-      href: ROUTES.SETTINGS_NOTIFICATIONS,
-    },
-    {
-      key: 'developer-portal',
-      title: t('nav.developer-portal-title'),
-      description: t('nav.developer-portal-description'),
-      icon: <Code {...iconAttr} />,
-      href: ROUTES.SETTINGS_DEVELOPER_PORTAL,
-    },
   ];
 
   return (
