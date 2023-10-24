4787e4d0f33bfd3562e72ccbb885e22c96bff5b9
Nuno Carvalho <nunoormaker21@gmail.com>
2023-10-24 22:12:50 +0100

    fix: lint

--- src/components/features/explore/tabs/all-tab/all-tab.tsx
+++ src/components/features/explore/tabs/all-tab/all-tab.tsx
@@ -67,9 +67,7 @@ export function AllTab({
           title={t('featured-credentials.title')}
           caption={t('featured-credentials.caption')}
           action={
-            <Button
-              onClick={() => router.push(ROUTES.EXPLORE_EARN)}
-            >
+            <Button onClick={() => router.push(ROUTES.EXPLORE_EARN)}>
               {t('featured-credentials.see-more')}
             </Button>
           }
@@ -88,9 +86,7 @@ export function AllTab({
             caption={`${t('featured-passes.caption')}`}
             action={
               passes.length > 0 && (
-                <Button
-                  onClick={() => router.push(ROUTES.EXPLORE_PASSES)}
-                >
+                <Button onClick={() => router.push(ROUTES.EXPLORE_PASSES)}>
                   {t('featured-passes.see-more')}
                 </Button>
               )
@@ -111,9 +107,7 @@ export function AllTab({
           title={t('featured-data-models.title')}
           caption={t('featured-data-models.caption')}
           action={
-            <Button
-              onClick={() => router.push(ROUTES.EXPLORE_ISSUE)}
-            >
+            <Button onClick={() => router.push(ROUTES.EXPLORE_ISSUE)}>
               {t('featured-data-models.see-more')}
             </Button>
           }
@@ -129,9 +123,7 @@ export function AllTab({
           title={t('featured-organizations.title')}
           caption={t('featured-organizations.caption')}
           action={
-            <Button
-              onClick={() => router.push(ROUTES.EXPLORE_ORGANIZATIONS)}
-            >
+            <Button onClick={() => router.push(ROUTES.EXPLORE_ORGANIZATIONS)}>
               {t('featured-organizations.see-more')}
             </Button>
           }
