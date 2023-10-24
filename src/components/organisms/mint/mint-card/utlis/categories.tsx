078fd240907dbed201465fc4eb5b3629d24f1d17
Nuno Carvalho <nunoormaker21@gmail.com>
2023-10-24 15:24:16 +0100

    chore: remove Mint as NFT button

--- src/components/organisms/mint/mint-card/utlis/categories.tsx
+++ src/components/organisms/mint/mint-card/utlis/categories.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react';
 
 import { Chip } from '@mui/material';
 
-import { Subjects } from '../index';
 import { MoreList } from './drop-down-list';
 
-export const Categories = ({ mintProcessStatus, categories }) => {
+export const Categories = ({ categories }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -16,53 +15,27 @@ export const Categories = ({ mintProcessStatus, categories }) => {
   };
 
   if (categories.length > 0) {
-    if (mintProcessStatus === Subjects.default) {
-      return (
-        <>
-          {categories.slice(0, 1).map((category: string) => (
-            <Chip key={category} label={category} size="small" />
-          ))}
-          {categories.length > 1 && (
-            <Chip
-              key={'more'}
-              label={'+' + (categories.length - 1)}
-              size="small"
-              aria-label="more"
-              id="long-button"
-              aria-controls={open ? 'long-menu' : undefined}
-              aria-expanded={open ? 'true' : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-            />
-          )}
-          {MoreList({ anchorEl, open, handleClose, categories, skip: 1 })}
-        </>
-      );
-    }
-
-    if (mintProcessStatus === Subjects.alreadyMinted) {
-      return (
-        <>
-          {categories.slice(0, 2).map((category: string) => (
-            <Chip key={category} label={category} size="small" />
-          ))}
-          {categories.length > 2 && (
-            <Chip
-              key={'more'}
-              label={'+' + (categories.length - 2)}
-              size="small"
-              aria-label="more"
-              id="long-button"
-              aria-controls={open ? 'long-menu' : undefined}
-              aria-expanded={open ? 'true' : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-            />
-          )}
-          {MoreList({ anchorEl, open, handleClose, categories, skip: 2 })}
-        </>
-      );
-    }
+    return (
+      <>
+        {categories.slice(0, 2).map((category: string) => (
+          <Chip key={category} label={category} size="small" />
+        ))}
+        {categories.length > 2 && (
+          <Chip
+            key={'more'}
+            label={'+' + (categories.length - 2)}
+            size="small"
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? 'long-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          />
+        )}
+        {MoreList({ anchorEl, open, handleClose, categories, skip: 2 })}
+      </>
+    );
   }
 
   return null;
